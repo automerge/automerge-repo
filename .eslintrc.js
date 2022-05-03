59e95c7db3cecd5c39e9e3fbf3308c0bb58f7843
Peter van Hardenberg <pvh@pvh.ca>
2022-05-03 16:10:20 -0700

    eslint file added

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'max-classes-per-file': 0,
+    semi: ['error', 'never'],
+  },
+};
