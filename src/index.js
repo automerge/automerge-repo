1bbc60cd64c99e128b26d1ff217e6fb87452eb81
Martin Kleppmann <martin@kleppmann.com>
2022-06-09 17:55:12 +0200

    Export the things needed by the storage peer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import BrowserRepo from './BrowserRepo.js'
+//import BrowserRepo from './BrowserRepo.js'
 import Repo from './Repo.js'
+import Network from './network/Network.js'
+import StorageSubsystem from './storage/StorageSubsystem.js'
+import DependencyCollectionSynchronizer from './synchronizer/CollectionSynchronizer.js'
 
 // These will all move to plugins as things settle.
-import LocalForageStorageAdapter from './storage/interfaces/LocalForageStorageAdapter.js'
-import BroadcastChannelNetworkAdapter from './network/interfaces/BroadcastChannelNetworkAdapter.js'
-import LocalFirstRelayNetworkAdapter from './network/interfaces/LocalFirstRelayNetworkAdapter.js'
+//import LocalForageStorageAdapter from './storage/interfaces/LocalForageStorageAdapter.js'
+//import BroadcastChannelNetworkAdapter from './network/interfaces/BroadcastChannelNetworkAdapter.js'
+//import LocalFirstRelayNetworkAdapter from './network/interfaces/LocalFirstRelayNetworkAdapter.js'
 
-export { BrowserRepo, Repo, LocalForageStorageAdapter, BroadcastChannelNetworkAdapter, LocalFirstRelayNetworkAdapter }
+export { Repo, Network, StorageSubsystem, DependencyCollectionSynchronizer }
+//BrowserRepo, LocalForageStorageAdapter, BroadcastChannelNetworkAdapter, LocalFirstRelayNetworkAdapter }
