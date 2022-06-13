4e9b7bb9871ea36369e8740f155699c0eb07f874
Peter van Hardenberg <pvh@pvh.ca>
2022-06-13 10:38:25 -0700

    comment out some browser-specific exports. we'll need a more robust solution for this soon.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import Network from './network/Network.js'
 import StorageSubsystem from './storage/StorageSubsystem.js'
 import DependencyCollectionSynchronizer from './synchronizer/CollectionSynchronizer.js'
 
-// These will all move to plugins as things settle.
-//import LocalForageStorageAdapter from './storage/interfaces/LocalForageStorageAdapter.js'
-//import BroadcastChannelNetworkAdapter from './network/interfaces/BroadcastChannelNetworkAdapter.js'
-//import LocalFirstRelayNetworkAdapter from './network/interfaces/LocalFirstRelayNetworkAdapter.js'
-
 export { Repo, Network, StorageSubsystem, DependencyCollectionSynchronizer }
-//BrowserRepo, LocalForageStorageAdapter, BroadcastChannelNetworkAdapter, LocalFirstRelayNetworkAdapter }
