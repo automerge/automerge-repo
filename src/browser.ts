79d8dca59c8a9d253a7a81e50cf9df98c0ab1284
Blaine Cook <romeda@gmail.com>
2022-08-30 22:52:29 -0700

    add missing memorystorageadapter

--- src/browser.ts
+++ src/browser.ts
@@ -7,5 +7,6 @@ import Network from './network/Network'
 import StorageSubsystem from './storage/StorageSubsystem'
 import DependencyCollectionSynchronizer from './synchronizer/CollectionSynchronizer'
 import DocHandle from './DocHandle'
+import MemoryStorageAdapter from './storage/interfaces/MemoryStorageAdapter'
 
-export { BrowserRepo, LocalForageStorageAdapter, BroadcastChannelNetworkAdapter, BrowserWebSocketClientAdapter, Repo, Network, StorageSubsystem, DependencyCollectionSynchronizer, DocHandle }
+export { BrowserRepo, LocalForageStorageAdapter, BroadcastChannelNetworkAdapter, MemoryStorageAdapter, BrowserWebSocketClientAdapter, Repo, Network, StorageSubsystem, DependencyCollectionSynchronizer, DocHandle }
