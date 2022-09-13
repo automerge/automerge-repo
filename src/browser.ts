04876ce96d5f4e72a53a2fc3191e52e2ae1acc92
HerbCaudill <herb@devresults.com>
2022-09-13 18:50:47 +0200

    index, browser: add LocalFirstRelay, sort imports

--- src/browser.ts
+++ src/browser.ts
@@ -1,23 +1,25 @@
-import Repo from "./Repo.js"
-import MemoryStorageAdapter from "./storage/interfaces/MemoryStorageAdapter.js"
-import LocalForageStorageAdapter from "./storage/interfaces/LocalForageStorageAdapter.js"
+import DocCollection from "./DocCollection.js"
+import DocHandle from "./DocHandle.js"
 import BroadcastChannelNetworkAdapter from "./network/interfaces/BroadcastChannelNetworkAdapter.js"
 import BrowserWebSocketClientAdapter from "./network/interfaces/BrowserWebSocketClientAdapter.js"
-import DocCollection from "./DocCollection.js"
+import LocalFirstRelayNetworkAdapter from "./network/interfaces/LocalFirstRelayNetworkAdapter.js"
 import Network from "./network/Network.js"
+import Repo from "./Repo.js"
+import LocalForageStorageAdapter from "./storage/interfaces/LocalForageStorageAdapter.js"
+import MemoryStorageAdapter from "./storage/interfaces/MemoryStorageAdapter.js"
 import StorageSubsystem from "./storage/StorageSubsystem.js"
 import DependencyCollectionSynchronizer from "./synchronizer/CollectionSynchronizer.js"
-import DocHandle from "./DocHandle.js"
 
 export {
-  Repo,
-  MemoryStorageAdapter,
-  LocalForageStorageAdapter,
   BroadcastChannelNetworkAdapter,
   BrowserWebSocketClientAdapter,
-  DocCollection,
-  Network,
-  StorageSubsystem,
   DependencyCollectionSynchronizer,
+  DocCollection,
   DocHandle,
+  LocalFirstRelayNetworkAdapter,
+  LocalForageStorageAdapter,
+  MemoryStorageAdapter,
+  Network,
+  Repo,
+  StorageSubsystem,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-import Repo from "./Repo.js"
 import DocCollection from "./DocCollection.js"
+import DocHandle, { DocHandleEventArg } from "./DocHandle.js"
+import LocalFirstRelayNetworkAdapter from "./network/interfaces/LocalFirstRelayNetworkAdapter.js"
+import { NodeWSServerAdapter } from "./network/interfaces/NodeWSServerAdapter.js"
 import Network from "./network/Network.js"
+import Repo from "./Repo.js"
+import { NodeFSStorageAdapter } from "./storage/interfaces/NodeFSStorageAdapter.js"
 import StorageSubsystem from "./storage/StorageSubsystem.js"
 import DependencyCollectionSynchronizer from "./synchronizer/CollectionSynchronizer.js"
-import DocHandle, { DocHandleEventArg } from "./DocHandle.js"
-import { NodeWSServerAdapter } from "./network/interfaces/NodeWSServerAdapter.js"
-import { NodeFSStorageAdapter } from "./storage/interfaces/NodeFSStorageAdapter.js"
 
 export {
-  Repo,
-  NodeWSServerAdapter,
-  NodeFSStorageAdapter,
-  DocCollection,
-  Network,
-  StorageSubsystem,
   DependencyCollectionSynchronizer,
+  DocCollection,
   DocHandle,
   DocHandleEventArg,
+  LocalFirstRelayNetworkAdapter,
+  Network,
+  NodeFSStorageAdapter,
+  NodeWSServerAdapter,
+  Repo,
+  StorageSubsystem,
 }
