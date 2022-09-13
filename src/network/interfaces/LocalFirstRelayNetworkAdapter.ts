b687d0cf39ebc0adac19c487f4f4739cb51df68c
HerbCaudill <herb@devresults.com>
2022-09-13 18:51:25 +0200

    LocalFirstRelayNetworkAdapter.ts: fix import

--- src/network/interfaces/LocalFirstRelayNetworkAdapter.ts
+++ src/network/interfaces/LocalFirstRelayNetworkAdapter.ts
@@ -1,10 +1,13 @@
-import { Client } from '@localfirst/relay-client'
-import EventEmitter from 'eventemitter3'
-import { NetworkAdapter, NetworkAdapterEvents } from '../Network.js'
-import { WebSocket } from 'isomorphic-ws'
+import { Client } from "@localfirst/relay-client"
+import EventEmitter from "eventemitter3"
+import { NetworkAdapter, NetworkAdapterEvents } from "../Network.js"
+import WebSocket from "isomorphic-ws"
 
-class LocalFirstRelayNetworkAdapter extends EventEmitter<NetworkAdapterEvents> implements NetworkAdapter {
-  url
+class LocalFirstRelayNetworkAdapter
+  extends EventEmitter<NetworkAdapterEvents>
+  implements NetworkAdapter
+{
+  url: string
   client?: Client
 
   constructor(url: string) {
@@ -20,12 +23,12 @@ class LocalFirstRelayNetworkAdapter extends EventEmitter<NetworkAdapterEvents> i
       send: (uint8message: Uint8Array) => {
         const message = uint8message.buffer.slice(
           uint8message.byteOffset,
-          uint8message.byteOffset + uint8message.byteLength,
+          uint8message.byteOffset + uint8message.byteLength
         )
         socket.send(message)
       },
     }
-    this.emit('peer-candidate', { peerId, channelId, connection })
+    this.emit("peer-candidate", { peerId, channelId, connection })
   }
 
   connect(peerId: string) {
@@ -34,18 +37,22 @@ class LocalFirstRelayNetworkAdapter extends EventEmitter<NetworkAdapterEvents> i
       url: this.url,
     })
 
-    this.client.on('peer.connect', (ev) => {
+    this.client.on("peer.connect", (ev) => {
       const documentId: string = ev.detail.documentId
       const userName: string = ev.detail.userName
       const socket: WebSocket = ev.detail.socket
 
-      socket.binaryType = 'arraybuffer'
+      socket.binaryType = "arraybuffer"
       this.announceConnection(documentId, userName, socket)
 
       // listen for messages
       socket.onmessage = (e) => {
         const message = new Uint8Array(e.data as ArrayBuffer)
-        this.emit('message', { senderId: userName, channelId: documentId, message })
+        this.emit("message", {
+          senderId: userName,
+          channelId: documentId,
+          message,
+        })
       }
     })
   }
