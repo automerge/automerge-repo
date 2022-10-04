25ee775ab71d96521b798a10fbd969a74561442c
Peter van Hardenberg <pvh@pvh.ca>
2022-10-04 14:00:58 -0700

    exclude the right dep optimize thing

--- packages/automerge-repo-react-demo/vite.config.ts
+++ packages/automerge-repo-react-demo/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
 
 export default defineConfig({
   plugins: [topLevelAwait(), react(), wasm()],
@@ -11,6 +11,6 @@ export default defineConfig({
     // versions of the JS wrapper. This causes problems because the JS
     // wrapper has a module level variable to track JS side heap
     // allocations, initializing this twice causes horrible breakage
-    exclude: ["automerge-wasm"],
+    exclude: ["@automerge/automerge-wasm"],
   },
-});
+})
