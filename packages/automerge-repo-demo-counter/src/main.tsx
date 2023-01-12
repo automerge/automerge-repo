130cebc85cd42815589de60490c340b5e7b17790
HerbCaudill <herb@devresults.com>
2023-01-12 18:07:03 +0100

    demo-counter//main.tsx: add sync server

--- packages/automerge-repo-demo-counter/src/main.tsx
+++ packages/automerge-repo-demo-counter/src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import { Repo } from 'automerge-repo'
-import { BroadcastChannelNetworkAdapter } from 'automerge-repo-network-broadcastchannel'
-import { LocalForageStorageAdapter } from 'automerge-repo-storage-localforage'
-import { RepoContext } from 'automerge-repo-react-hooks'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { App } from "./App"
+import { Repo } from "automerge-repo"
+import { BroadcastChannelNetworkAdapter } from "automerge-repo-network-broadcastchannel"
+import { LocalForageStorageAdapter } from "automerge-repo-storage-localforage"
+import { RepoContext } from "automerge-repo-react-hooks"
+import { BrowserWebSocketClientAdapter } from "automerge-repo-network-websocket"
 
 const repo = new Repo({
-  network: [new BroadcastChannelNetworkAdapter()],
+  network: [
+    new BroadcastChannelNetworkAdapter(),
+    new BrowserWebSocketClientAdapter("wss://localhost:3030"),
+  ],
   storage: new LocalForageStorageAdapter(),
 })
 
@@ -17,7 +21,7 @@ if (!appDocId) {
   localStorage.appDocId = appDocId = handle.documentId
 }
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <RepoContext.Provider value={repo}>
     <React.StrictMode>
       <App documentId={appDocId} />
