fb014aaf81e4a14c8acdfef574d329b29448bab9
HerbCaudill <herb@devresults.com>
2023-01-12 18:57:15 +0100

    packages/automerge-repo//index.ts: export directly

--- packages/automerge-repo/src/index.ts
+++ packages/automerge-repo/src/index.ts
@@ -1,38 +1,24 @@
-import { DocCollection } from "./DocCollection.js"
-import {
+export { DocCollection } from "./DocCollection.js"
+export {
   DocHandle,
   DocHandleChangeEvent,
   DocHandlePatchEvent,
   DocumentId,
 } from "./DocHandle.js"
-import {
-  DecodedMessage,
-  NetworkAdapter,
-  NetworkAdapterEvents,
-  NetworkSubsystem,
-  Peer,
-  PeerId,
-  ChannelId,
-} from "./network/NetworkSubsystem.js"
-import { Repo } from "./Repo.js"
-import { StorageAdapter, StorageSubsystem } from "./storage/StorageSubsystem.js"
-import { CollectionSynchronizer as DependencyCollectionSynchronizer } from "./synchronizer/CollectionSynchronizer.js"
-
 export {
-  Repo,
-  DependencyCollectionSynchronizer,
-  DocCollection,
-  DocHandle,
-  DocumentId,
-  DocHandlePatchEvent,
-  DocHandleChangeEvent,
+  ChannelId,
   DecodedMessage,
-  NetworkSubsystem,
   NetworkAdapter,
   NetworkAdapterEvents,
+  NetworkSubsystem,
   Peer,
   PeerId,
-  ChannelId,
-  StorageAdapter,
+} from "./network/NetworkSubsystem.js"
+export { Repo } from "./Repo.js"
+export {
+  StorageAdapter, //
   StorageSubsystem,
-}
+} from "./storage/StorageSubsystem.js"
+export {
+  CollectionSynchronizer as DependencyCollectionSynchronizer, //
+} from "./synchronizer/CollectionSynchronizer.js"
