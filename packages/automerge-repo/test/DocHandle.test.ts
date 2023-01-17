2e85feaf605fc4e170c6a2633294038bfb0d5b86
HerbCaudill <herb@devresults.com>
2023-01-17 18:01:44 +0100

    DocHandle.test: fix imports

--- packages/automerge-repo/test/DocHandle.test.ts
+++ packages/automerge-repo/test/DocHandle.test.ts
@@ -1,6 +1,7 @@
 import assert from "assert"
-import { DocHandle, DocumentId } from "../src/DocHandle"
+import { DocHandle } from "../src/DocHandle"
 import * as Automerge from "@automerge/automerge"
+import { DocumentId } from "../src/types"
 
 interface TestDoc {
   foo: string
@@ -17,19 +18,19 @@ describe("DocHandle", () => {
     assert(handle.ready() === false)
     // updateDoc is called by the sync / storage systems
     // this call is just to simulate loading
-    handle.updateDoc((doc) => Automerge.change(doc, (d) => (d.foo = "bar")))
+    handle.updateDoc(doc => Automerge.change(doc, d => (d.foo = "bar")))
     assert(handle.ready() === true)
   })
 
-  it("should not return a value until ready()", (done) => {
+  it("should not return a value until ready()", done => {
     const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
     assert(handle.ready() === false)
     let tooSoon = true
-    handle.updateDoc((doc) => {
+    handle.updateDoc(doc => {
       tooSoon = false
-      return Automerge.change(doc, (d) => (d.foo = "bar"))
+      return Automerge.change(doc, d => (d.foo = "bar"))
     })
-    handle.value().then((doc) => {
+    handle.value().then(doc => {
       try {
         assert(tooSoon === false)
         assert(handle.ready() === true)
@@ -40,12 +41,12 @@ describe("DocHandle", () => {
       }
     })
   })
-  
-  it("should return syncValue following an incremental load on an existing document", (done) => {
+
+  it("should return syncValue following an incremental load on an existing document", done => {
     const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
     assert(handle.ready() === false)
-    
-    handle.syncValue().then((doc) => {
+
+    handle.syncValue().then(doc => {
       try {
         assert(handle.ready() === true)
         assert(doc.foo === "bar")
@@ -55,16 +56,23 @@ describe("DocHandle", () => {
       }
     })
 
-    handle.loadIncremental(Automerge.save(Automerge.change<{foo: string}>(Automerge.init(), (d) => (d.foo = "bar"))))
+    handle.loadIncremental(
+      Automerge.save(
+        Automerge.change<{ foo: string }>(
+          Automerge.init(),
+          d => (d.foo = "bar")
+        )
+      )
+    )
   })
 
-  it("should block changes until ready()", (done) => {
+  it("should block changes until ready()", done => {
     const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
     assert(handle.ready() === false)
     let tooSoon = true
-    handle.updateDoc((doc) => {
+    handle.updateDoc(doc => {
       tooSoon = false
-      return Automerge.change(doc, (d) => (d.foo = "bar"))
+      return Automerge.change(doc, d => (d.foo = "bar"))
     })
     handle.change(() => {
       try {
@@ -77,7 +85,7 @@ describe("DocHandle", () => {
     })
   })
 
-  it("should emit a change message when changes happen", (done) => {
+  it("should emit a change message when changes happen", done => {
     const handle = new DocHandle<TestDoc>(
       "test-document-id" as DocumentId,
       true
@@ -86,12 +94,12 @@ describe("DocHandle", () => {
       assert(handle.doc.foo === "bar")
       done()
     })
-    handle.change((doc) => {
+    handle.change(doc => {
       doc.foo = "bar"
     })
   })
 
-  it("should not emit a change message if no change happens via updateDoc", (done) => {
+  it("should not emit a change message if no change happens via updateDoc", done => {
     const handle = new DocHandle<TestDoc>(
       "test-document-id" as DocumentId,
       true
@@ -99,13 +107,13 @@ describe("DocHandle", () => {
     handle.on("change", () => {
       done(new Error("shouldn't have changed"))
     })
-    handle.updateDoc((d) => {
+    handle.updateDoc(d => {
       setTimeout(done, 0)
       return d
     })
   })
 
-  it("should emit a patch message when changes happen", (done) => {
+  it("should emit a patch message when changes happen", done => {
     const handle = new DocHandle<TestDoc>(
       "test-document-id" as DocumentId,
       true
@@ -121,12 +129,12 @@ describe("DocHandle", () => {
       assert(after.foo === "bar", "after message didn't match")
       done()
     })
-    handle.change((doc) => {
+    handle.change(doc => {
       doc.foo = "bar"
     })
   })
 
-  it("should not emit a patch message if no change happens", (done) => {
+  it("should not emit a patch message if no change happens", done => {
     const handle = new DocHandle<TestDoc>(
       "test-document-id" as DocumentId,
       true
