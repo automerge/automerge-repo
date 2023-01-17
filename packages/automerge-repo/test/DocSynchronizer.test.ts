1078c712114459f84afd15abab6d1d4c19ef907a
HerbCaudill <herb@devresults.com>
2023-01-17 18:02:03 +0100

    DocSynchronizer.test: fix imports

--- packages/automerge-repo/test/DocSynchronizer.test.ts
+++ packages/automerge-repo/test/DocSynchronizer.test.ts
@@ -1,14 +1,14 @@
 import assert from "assert"
-import { DocHandle, DocumentId } from "../src/DocHandle"
+import { DocumentId, PeerId } from "../src/types"
+import { DocHandle } from "../src/DocHandle"
 import { DocSynchronizer } from "../src/synchronizer/DocSynchronizer"
-import { PeerId } from "../src/network/NetworkSubsystem"
 
 describe("DocSynchronizer", () => {
-  let handle: DocHandle<{foo: string}>
+  let handle: DocHandle<{ foo: string }>
   let docSynchronizer: DocSynchronizer
 
   beforeEach(() => {
-    handle = new DocHandle<{foo: string}>("synced-doc" as DocumentId, true)
+    handle = new DocHandle<{ foo: string }>("synced-doc" as DocumentId, true)
     docSynchronizer = new DocSynchronizer(handle)
   })
 
@@ -16,37 +16,35 @@ describe("DocSynchronizer", () => {
     assert(docSynchronizer.handle === handle)
   })
 
-  it("should emit a syncMessage when beginSync is called", (done) => {
+  it("should emit a syncMessage when beginSync is called", done => {
     docSynchronizer.once("message", () => done())
     docSynchronizer.beginSync("imaginary-peer-id" as PeerId)
   })
 
-  it("should emit a syncMessage to peers when the handle is updated", (done) => {
+  it("should emit a syncMessage to peers when the handle is updated", done => {
     docSynchronizer.beginSync("imaginary-peer-id" as PeerId)
     docSynchronizer.once("message", () => done())
-    handle.change((doc) => {
+    handle.change(doc => {
       doc.foo = "bar"
     })
   })
 
-  it("should emit a syncMessage to peers when the peer is removed, then re-added", (done) => {
+  it("should emit a syncMessage to peers when the peer is removed, then re-added", done => {
     docSynchronizer.beginSync("imaginary-peer-id-2" as PeerId).then(() => {
-      handle.change((doc) => {
+      handle.change(doc => {
         doc.foo = "bar"
       })
       docSynchronizer.endSync("imaginary-peer-id-2" as PeerId)
-      docSynchronizer.beginSync("imaginary-peer-id-2" as PeerId).then(
-        () => {
-          docSynchronizer.once("message", (event) => {
-            if (event.targetId === "imaginary-peer-id-2") {
-              done()
-            }
-          })
-          handle.change((doc) => {
-            doc.foo = "baz"
-          })
-        }
-      )
+      docSynchronizer.beginSync("imaginary-peer-id-2" as PeerId).then(() => {
+        docSynchronizer.once("message", event => {
+          if (event.targetId === "imaginary-peer-id-2") {
+            done()
+          }
+        })
+        handle.change(doc => {
+          doc.foo = "baz"
+        })
+      })
     })
   })
 })
