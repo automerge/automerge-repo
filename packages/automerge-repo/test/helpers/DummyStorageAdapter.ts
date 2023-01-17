82b6b5144e4faabd072d66430cf1357f076437cb
HerbCaudill <herb@devresults.com>
2023-01-18 00:42:41 +0100

    helpers/DummyStorageAdapter:

--- packages/automerge-repo/test/helpers/DummyStorageAdapter.ts
+++ packages/automerge-repo/test/helpers/DummyStorageAdapter.ts
@@ -3,17 +3,17 @@ import { DocumentId, StorageAdapter } from "../../src"
 export class DummyStorageAdapter implements StorageAdapter {
   #data: Record<DocumentId, Uint8Array> = {}
 
-  load(docId: string) {
+  load(docId: DocumentId) {
     return new Promise<Uint8Array | null>(resolve =>
       resolve(this.#data[docId] || null)
     )
   }
 
-  save(docId: string, binary: Uint8Array) {
+  save(docId: DocumentId, binary: Uint8Array) {
     this.#data[docId] = binary
   }
 
-  remove(docId: string) {
+  remove(docId: DocumentId) {
     delete this.#data[docId]
   }
 }
