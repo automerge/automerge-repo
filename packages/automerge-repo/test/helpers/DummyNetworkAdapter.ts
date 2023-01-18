4f8d8f4b81449bb05c59667b9cf8e57f23833308
HerbCaudill <herb@devresults.com>
2023-01-18 01:11:48 +0100

    helpers/DummyNetworkAdapter:

--- packages/automerge-repo/test/helpers/DummyNetworkAdapter.ts
+++ packages/automerge-repo/test/helpers/DummyNetworkAdapter.ts
@@ -1,23 +1,12 @@
 import EventEmitter from "eventemitter3"
-import {
-  ChannelId,
-  NetworkAdapter,
-  NetworkAdapterEvents,
-  PeerId,
-} from "../../src/types"
+import { NetworkAdapter, NetworkAdapterEvents } from "../../src"
 
 export class DummyNetworkAdapter
   extends EventEmitter<NetworkAdapterEvents>
   implements NetworkAdapter
 {
-  sendMessage(
-    peerId: PeerId,
-    channelId: ChannelId,
-    message: Uint8Array
-  ): void {}
+  sendMessage() {}
   connect(_: string) {}
-
   join(_: string) {}
-
   leave(_: string) {}
 }
