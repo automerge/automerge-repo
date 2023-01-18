2ae32630cd78cc97ba237f57c06f87f8f1e3c3b3
HerbCaudill <herb@devresults.com>
2023-01-18 01:26:14 +0100

    DocHandle.test:

--- packages/automerge-repo/test/DocHandle.test.ts
+++ packages/automerge-repo/test/DocHandle.test.ts
@@ -1,7 +1,8 @@
-import assert from "assert"
-import { DocHandle } from "../src/DocHandle"
 import * as Automerge from "@automerge/automerge"
-import { DocumentId } from "../src/types"
+
+import assert from "assert"
+import { DocHandle, DocumentId } from "../src/"
+import { eventFired } from "./helpers/eventFired"
 
 interface TestDoc {
   foo: string
@@ -22,122 +23,96 @@ describe("DocHandle", () => {
     assert(handle.ready() === true)
   })
 
-  it("should not return a value until ready()", done => {
+  it("should not return a value until ready()", async () => {
     const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
     assert(handle.ready() === false)
-    let tooSoon = true
+    let tooSoon = true as boolean
+
     handle.updateDoc(doc => {
       tooSoon = false
       return Automerge.change(doc, d => (d.foo = "bar"))
     })
-    handle.value().then(doc => {
-      try {
-        assert(tooSoon === false)
-        assert(handle.ready() === true)
-        assert(doc.foo === "bar")
-        done()
-      } catch (e) {
-        done(e)
-      }
-    })
+
+    const doc = await handle.value()
+
+    assert(tooSoon === false)
+    assert(handle.ready() === true)
+    assert(doc.foo === "bar")
   })
+})
 
-  it("should return syncValue following an incremental load on an existing document", done => {
-    const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
-    assert(handle.ready() === false)
+it("should return syncValue following an incremental load on an existing document", async () => {
+  const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
+  assert(handle.ready() === false)
 
-    handle.syncValue().then(doc => {
-      try {
-        assert(handle.ready() === true)
-        assert(doc.foo === "bar")
-        done()
-      } catch (e) {
-        done(e)
-      }
-    })
-
-    handle.loadIncremental(
-      Automerge.save(
-        Automerge.change<{ foo: string }>(
-          Automerge.init(),
-          d => (d.foo = "bar")
-        )
-      )
+  handle.loadIncremental(
+    Automerge.save(
+      Automerge.change<{ foo: string }>(Automerge.init(), d => (d.foo = "bar"))
     )
-  })
+  )
 
-  it("should block changes until ready()", done => {
-    const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
-    assert(handle.ready() === false)
-    let tooSoon = true
-    handle.updateDoc(doc => {
-      tooSoon = false
-      return Automerge.change(doc, d => (d.foo = "bar"))
-    })
-    handle.change(() => {
-      try {
-        assert(tooSoon === false)
-        assert(handle.ready() === true)
-        done()
-      } catch (e) {
-        done(e)
-      }
-    })
-  })
+  const doc = await handle.syncValue()
+  assert(handle.ready() === true)
+  assert(doc.foo === "bar")
+})
 
-  it("should emit a change message when changes happen", done => {
-    const handle = new DocHandle<TestDoc>(
-      "test-document-id" as DocumentId,
-      true
-    )
-    handle.on("change", ({ handle }) => {
-      assert(handle.doc.foo === "bar")
+it("should block changes until ready()", done => {
+  const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId)
+  assert(handle.ready() === false)
+  let tooSoon = true
+  handle.updateDoc(doc => {
+    tooSoon = false
+    return Automerge.change(doc, d => (d.foo = "bar"))
+  })
+  handle.change(() => {
+    try {
+      assert(tooSoon === false)
+      assert(handle.ready() === true)
       done()
-    })
-    handle.change(doc => {
-      doc.foo = "bar"
-    })
+    } catch (e) {
+      done(e)
+    }
+  })
+})
+
+it("should emit a change message when changes happen", async () => {
+  const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId, true)
+  handle.change(doc => {
+    doc.foo = "bar"
   })
 
-  it("should not emit a change message if no change happens via updateDoc", done => {
-    const handle = new DocHandle<TestDoc>(
-      "test-document-id" as DocumentId,
-      true
-    )
-    handle.on("change", () => {
-      done(new Error("shouldn't have changed"))
-    })
-    handle.updateDoc(d => {
-      setTimeout(done, 0)
-      return d
-    })
+  await eventFired(handle, "change")
+  assert(handle.doc.foo === "bar")
+})
+
+it("should not emit a change message if no change happens via updateDoc", done => {
+  const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId, true)
+  handle.on("change", () => {
+    done(new Error("shouldn't have changed"))
+  })
+  handle.updateDoc(d => {
+    setTimeout(done, 0)
+    return d
+  })
+})
+
+it("should emit a patch message when changes happen", async () => {
+  const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId, true)
+  handle.change(doc => {
+    doc.foo = "bar"
   })
 
-  it("should emit a patch message when changes happen", done => {
-    const handle = new DocHandle<TestDoc>(
-      "test-document-id" as DocumentId,
-      true
-    )
-    handle.on("patch", ({ after }) => {
-      assert(after.foo === "bar")
-      done()
-    })
-    handle.change(doc => {
-      doc.foo = "bar"
-    })
-  })
+  const { after } = await eventFired(handle, "patch")
+  assert(after.foo === "bar")
+})
 
-  it("should not emit a patch message if no change happens", done => {
-    const handle = new DocHandle<TestDoc>(
-      "test-document-id" as DocumentId,
-      true
-    )
-    handle.on("patch", () => {
-      done(new Error("shouldn't have patched"))
-    })
-    handle.change(() => {
-      // do nothing
-      setTimeout(done, 0)
-    })
+it("should not emit a patch message if no change happens", done => {
+  const handle = new DocHandle<TestDoc>("test-document-id" as DocumentId, true)
+  handle.on("patch", () => {
+    done(new Error("shouldn't have patched"))
+  })
+  handle.change(() => {
+    // do nothing
+    setTimeout(done, 0)
   })
 })
