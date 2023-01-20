7a13b60f02cdd15afb9a5465ea53d87ad51e7675
HerbCaudill <herb@devresults.com>
2023-01-20 14:03:39 +0100

    storage-localforage/index: StorageAdapter is abstract class

--- packages/automerge-repo-storage-localforage/src/index.ts
+++ packages/automerge-repo-storage-localforage/src/index.ts
@@ -1,14 +1,16 @@
 import * as localforage from "localforage"
 import { StorageAdapter } from "automerge-repo"
 
-// Saving & loading via localforage. Very naive but probably fine for blob-storage.
-export class LocalForageStorageAdapter implements StorageAdapter {
+/** Saving & loading via localforage. Very naive but probably fine for blob-storage.  */
+export class LocalForageStorageAdapter extends StorageAdapter {
   load(docId: string) {
     return localforage.getItem<Uint8Array>(docId)
   }
+
   save(docId: string, binary: Uint8Array) {
     localforage.setItem(docId, binary)
   }
+
   remove(docId: string) {
     localforage.removeItem(docId)
   }
