da0c4abba47f6dacb5c76e611ccbddc1a300acc5
HerbCaudill <herb@devresults.com>
2023-01-20 16:19:17 +0100

    helpers/headsAreSame:

--- packages/automerge-repo/src/helpers/headsAreSame.ts
+++ packages/automerge-repo/src/helpers/headsAreSame.ts
@@ -1,6 +1,7 @@
+import * as A from "@automerge/automerge"
 import { arraysAreEqual } from "./arraysAreEqual"
 
-const headsAreSame = <T>(a: A.Doc<T>, b: A.Doc<T>) => {
+export const headsAreSame = <T>(a: A.Doc<T>, b: A.Doc<T>) => {
   const aHeads = A.getHeads(a)
   const bHeads = A.getHeads(b)
   return arraysAreEqual(aHeads, bHeads)
