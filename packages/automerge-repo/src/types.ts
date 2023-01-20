8505c7529ca85f7e1615ec32c4f1b1f3a323665f
HerbCaudill <herb@devresults.com>
2023-01-20 14:01:55 +0100

    types: move types to DocHandle

--- packages/automerge-repo/src/types.ts
+++ packages/automerge-repo/src/types.ts
@@ -1,15 +1,3 @@
-import * as Automerge from "@automerge/automerge"
-import { DocHandle } from "./DocHandle"
-
 export type DocumentId = string & { __documentId: true }
 export type PeerId = string & { __peerId: false }
 export type ChannelId = string & { __channelId: false }
-
-export const HandleState = {
-  LOADING: "LOADING",
-  SYNCING: "SYNCING",
-  READY: "READY",
-} as const
-
-// avoiding enum https://maxheiber.medium.com/alternatives-to-typescript-enums-50e4c16600b1
-export type HandleState = typeof HandleState[keyof typeof HandleState]
