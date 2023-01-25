7b8c2943e820ac609374446598792771732b7905
HerbCaudill <herb@devresults.com>
2023-01-25 16:49:12 +0100

    demo-todo/App: add message when document not found

--- packages/automerge-repo-demo-todo/src/App.tsx
+++ packages/automerge-repo-demo-todo/src/App.tsx
@@ -44,114 +44,127 @@ export function App(props: { rootId: DocumentId }) {
     }
   }
 
-  if (!state) return null
-
   return (
     <>
       <div className="flex h-screen pt-2 pb-96 bg-primary-50">
         <div className="m-auto w-4/5 max-w-xl border border-neutral-300 shadow-md rounded-md bg-white">
-          <header>
-            <form
-              onSubmit={e => {
-                e.preventDefault()
-                if (!newTodoInput.current) return
+          {!state ? (
+            <>
+              <div className="p-5">
+                <p className="my-3">
+                  <span className="text-3xl">⚠️</span> Document not found.
+                </p>
+                <pre className="text-sm bg-gray-100 rounded p-3">
+                  id: {props.rootId}
+                </pre>
+              </div>
+            </>
+          ) : (
+            <>
+              <header>
+                <form
+                  onSubmit={e => {
+                    e.preventDefault()
+                    if (!newTodoInput.current) return
 
-                const newTodoText = newTodoInput.current.value.trim()
+                    const newTodoText = newTodoInput.current.value.trim()
 
-                // don't create empty todos
-                if (newTodoText.length === 0) return
+                    // don't create empty todos
+                    if (newTodoText.length === 0) return
 
-                const handle = repo.create<TodoData>()
-                const id = handle.documentId
-                handle.change(t => {
-                  t.id = id
-                  t.content = newTodoText
-                  t.completed = false
-                })
+                    const handle = repo.create<TodoData>()
+                    const id = handle.documentId
+                    handle.change(t => {
+                      t.id = id
+                      t.content = newTodoText
+                      t.completed = false
+                    })
 
-                // update state with new todo
-                changeState(s => {
-                  s.todos.push(id)
-                })
+                    // update state with new todo
+                    changeState(s => {
+                      s.todos.push(id)
+                    })
 
-                // clear input
-                newTodoInput.current.value = ""
-              }}
-            >
-              <input
-                className="w-full p-3 rounded-md"
-                placeholder="Add a new todo"
-                ref={newTodoInput}
-                autoFocus={true}
-              />
-            </form>
-          </header>
+                    // clear input
+                    newTodoInput.current.value = ""
+                  }}
+                >
+                  <input
+                    className="w-full p-3 rounded-md"
+                    placeholder="Add a new todo"
+                    ref={newTodoInput}
+                    autoFocus={true}
+                  />
+                </form>
+              </header>
 
-          {/* todos */}
-          <section>
-            <ul className="border-y divide-y divide-solid">
-              {state.todos.map(id => (
-                <Todo
-                  key={id}
-                  documentId={id}
-                  onDestroy={id => destroy(id)}
-                  filter={filter}
-                />
-              ))}
-            </ul>
-          </section>
+              {/* todos */}
+              <section>
+                <ul className="border-y divide-y divide-solid">
+                  {state.todos.map(id => (
+                    <Todo
+                      key={id}
+                      documentId={id}
+                      onDestroy={id => destroy(id)}
+                      filter={filter}
+                    />
+                  ))}
+                </ul>
+              </section>
 
-          {/* footer tools */}
-          <footer className="p-3 flex justify-between items-center text-sm">
-            {/* remaining count */}
-            {/* <span className="flex-1">
+              {/* footer tools */}
+              <footer className="p-3 flex justify-between items-center text-sm">
+                {/* remaining count */}
+                {/* <span className="flex-1">
               <strong>{incompleteCount}</strong>{" "}
               {pluralize(incompleteCount, "item")} left
             </span> */}
 
-            {/* filter */}
-            <ul className="flex-1 flex space-x-1 cursor-pointer">
-              {Object.keys(Filter).map(k => {
-                const key = k as Filter
-                const active = key === filter
+                {/* filter */}
+                <ul className="flex-1 flex space-x-1 cursor-pointer">
+                  {Object.keys(Filter).map(k => {
+                    const key = k as Filter
+                    const active = key === filter
 
-                const buttonStyle = cx({
-                  ["text-gray-500 hover:text-gray-700 px-3 py-2 font-medium text-sm rounded-md"]:
-                    !active,
-                  ["bg-gray-100 text-gray-700 px-3 py-2 font-medium text-sm rounded-md"]:
-                    active,
-                })
+                    const buttonStyle = cx({
+                      ["text-gray-500 hover:text-gray-700 px-3 py-2 font-medium text-sm rounded-md"]:
+                        !active,
+                      ["bg-gray-100 text-gray-700 px-3 py-2 font-medium text-sm rounded-md"]:
+                        active,
+                    })
 
-                return (
-                  <li className="leading-none" key={`filter-${key}`}>
-                    <button
-                      className={buttonStyle}
-                      onClick={e => {
-                        e.preventDefault()
-                        setFilter(key)
-                      }}
-                    >
-                      {key}
-                    </button>
-                  </li>
-                )
-              })}
-            </ul>
-            <div className="flex-1 text-right">
-              <button
-                className={cx(
-                  "leading-none border py-2 px-4 rounded-md",
-                  "hover:border-primary-600 hover:bg-primary-500 hover:text-white"
-                )}
-                onClick={e => {
-                  e.preventDefault()
-                  destroyCompleted()
-                }}
-              >
-                Clear completed
-              </button>
-            </div>
-          </footer>
+                    return (
+                      <li className="leading-none" key={`filter-${key}`}>
+                        <button
+                          className={buttonStyle}
+                          onClick={e => {
+                            e.preventDefault()
+                            setFilter(key)
+                          }}
+                        >
+                          {key}
+                        </button>
+                      </li>
+                    )
+                  })}
+                </ul>
+                <div className="flex-1 text-right">
+                  <button
+                    className={cx(
+                      "leading-none border py-2 px-4 rounded-md",
+                      "hover:border-primary-600 hover:bg-primary-500 hover:text-white"
+                    )}
+                    onClick={e => {
+                      e.preventDefault()
+                      destroyCompleted()
+                    }}
+                  >
+                    Clear completed
+                  </button>
+                </div>
+              </footer>
+            </>
+          )}
         </div>
       </div>
     </>
