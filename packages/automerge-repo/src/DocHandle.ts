f3eb93ff4de3ec7920e3fd6ec5092d9a79c38b46
Peter van Hardenberg <pvh@pvh.ca>
2023-02-15 11:17:39 -0800

    trivial change

--- packages/automerge-repo/src/DocHandle.ts
+++ packages/automerge-repo/src/DocHandle.ts
@@ -35,6 +35,7 @@ type HandleState = "loading" | "syncing" | "ready"
 
 /**
  * DocHandle is a wrapper around a single Automerge document that allows us to listen for changes.
+ *
  */
 export class DocHandle<T> extends EventEmitter<DocHandleEvents<T>> {
   doc: Automerge.Doc<T>
@@ -121,9 +122,9 @@ export class DocHandle<T> extends EventEmitter<DocHandleEvents<T>> {
   async value() {
     if (!this.ready()) {
       log(`[${this.documentId}]: value: (${this.state}, waiting for ready)`)
-      await new Promise((resolve) => this.once("ready", () => resolve(true)))
+      await new Promise(resolve => this.once("ready", () => resolve(true)))
     } else {
-      await new Promise((resolve) => setTimeout(() => resolve(true), 0))
+      await new Promise(resolve => setTimeout(() => resolve(true), 0))
     }
     log(`[${this.documentId}]: value:`, this.doc)
     return this.doc
@@ -136,12 +137,12 @@ export class DocHandle<T> extends EventEmitter<DocHandleEvents<T>> {
     log(`[${this.documentId}]: syncValue,`, this.doc)
     if (this.state == "loading") {
       log(`[${this.documentId}]: value: (${this.state}, waiting for syncing)`)
-      await new Promise((resolve) => {
+      await new Promise(resolve => {
         this.once("syncing", () => resolve(true))
         this.once("ready", () => resolve(true))
       })
     } else {
-      await new Promise((resolve) => setTimeout(() => resolve(true), 0))
+      await new Promise(resolve => setTimeout(() => resolve(true), 0))
     }
     log(`[${this.documentId}]: syncValue:`, this.doc)
     return this.doc
