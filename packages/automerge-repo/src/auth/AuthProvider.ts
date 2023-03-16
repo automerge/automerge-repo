c4c5ec2b95a84cb141ee33dc04d07ea5dc3ae7b6
HerbCaudill <herb@devresults.com>
2023-03-16 17:05:07 +0100

    auth/AuthProvider: comments

--- packages/automerge-repo/src/auth/AuthProvider.ts
+++ packages/automerge-repo/src/auth/AuthProvider.ts
@@ -10,12 +10,25 @@ export abstract class AuthProvider {
   /** Should we provide this document (and changes to it) to this peer when asked for it by ID? */
   okToSend: SharePolicy = NEVER
 
-  /** Should we accept changes to this document from this peer? */
+  /**
+   * Should we accept changes to this document from this peer?
+   *
+   * Note: This isn't useful for authorization, since the peer might be passing on changes authored
+   * by someone else. In most cases this will just return `true` (since by that point the peer has
+   * been authenticated).
+   */
   okToReceive: SharePolicy = NEVER
 }
 
 export type ValidAuthenticationResult = {
   isValid: true
+
+  /**
+   * An AuthProvider can optionally return a channel that the Repo should use for subsequent
+   * communication with this peer. (For example, in localfirst/auth two peers negotiate a shared
+   * secret and and use that to create an encrypted channel.)
+   */
+  channel?: WebSocket
 }
 
 export type InvalidAuthenticationResult = {
