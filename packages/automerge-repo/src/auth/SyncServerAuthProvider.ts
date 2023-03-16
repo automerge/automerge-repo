988d2b4386ff92fb416ce5597d68a5d5980d074d
HerbCaudill <herb@devresults.com>
2023-03-16 17:05:26 +0100

    rename to SyncServerAuthProvider

--- packages/automerge-repo/src/auth/SyncServerAuthProvider.ts
+++ packages/automerge-repo/src/auth/SyncServerAuthProvider.ts
@@ -1,7 +1,7 @@
 import { ALWAYS, AuthProvider, NEVER, VALID } from "./AuthProvider"
 
 /** Just like the GenerousAuthProvider, but doesn't advertise anything */
-export class ServerAuthProvider extends AuthProvider {
+export class SyncServerAuthProvider extends AuthProvider {
   authenticate = async () => VALID
   okToAdvertise = NEVER
   okToSend = ALWAYS
