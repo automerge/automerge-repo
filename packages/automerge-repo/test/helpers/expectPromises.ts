a0e8595a104468fd4353b0965e9abc0a0b628adf
HerbCaudill <herb@devresults.com>
2023-03-17 11:31:53 +0100

    helpers/expectPromises: use existing `withTimeout` helper

--- packages/automerge-repo/test/helpers/expectPromises.ts
+++ packages/automerge-repo/test/helpers/expectPromises.ts
@@ -1,12 +1,6 @@
-import assert from "assert"
-import { pause } from "../../src/helpers/pause"
+import { withTimeout } from "../../src/helpers/withTimeout"
 
 export async function expectPromises(...promises: Promise<any>[]) {
   const timeout = 50
-  await Promise.race([
-    Promise.all(promises),
-    pause(timeout).then(() =>
-      assert.fail(`expected promises did not all resolve in ${timeout} ms`)
-    ),
-  ])
+  return withTimeout(Promise.all(promises), timeout)
 }
