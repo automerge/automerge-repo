22700be39831ec555021092038e613683ed222fb
Dylan MacKenzie <ecstaticmorse@gmail.com>
2023-04-25 14:05:59 -0700

    add a svelte store

---
+++ packages/automerge-repo-svelte-store/src/index.ts
@@ -0,0 +1,30 @@
+import type { Doc, ChangeFn } from '@automerge/automerge'
+import { setContext, getContext } from 'svelte'
+import { writable } from 'svelte/store'
+import { Repo, DocumentId, DocHandlePatchPayload } from 'automerge-repo'
+
+const ContextRepoKey = Symbol('svelte-context-automerge-repo')
+
+export function getContextRepo(): Repo {
+    return getContext<Repo>(ContextRepoKey)
+}
+
+export function setContextRepo(repo: Repo) {
+    setContext(ContextRepoKey, repo)
+}
+
+export function document<T>(documentId: DocumentId) {
+    const repo = getContextRepo()
+    const handle = repo.find<T>(documentId)
+    const { set, subscribe } = writable<Doc<T>>(null, () => {
+        const onPatch = (h: DocHandlePatchPayload<T>) => set(h.after)
+        handle.addListener('patch', onPatch)
+        return () => handle.removeListener('patch', onPatch)
+    })
+
+    return {
+        subscribe,
+        change: (fn: ChangeFn<T>) => { handle.change(fn) }
+    }
+}
+
