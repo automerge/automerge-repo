2c7425dcff2bd5e1eba3c717f0bbdd6077d3f0c7
Dylan MacKenzie <ecstaticmorse@gmail.com>
2023-04-25 14:05:29 -0700

    add a svelte counter task

---
+++ packages/automerge-repo-demo-counter-svelte/src/lib/Counter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { document } from "automerge-repo-svelte-store"
+  import { type DocumentId } from "automerge-repo"
+
+  export let documentId: DocumentId
+
+  interface HasCount {
+    count?: number
+  }
+
+  const doc = document<HasCount>(documentId)
+  const increment = () => {
+    doc.change((d: HasCount) => d.count = (d.count || 0) + 1)
+  }
+</script>
+
+<button on:click={increment}>
+  count is {$doc?.count || 0}
+</button>
---
+++ packages/automerge-repo-demo-counter-svelte/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [wasm(), topLevelAwait(), svelte()],
+
+  worker: {
+    format: "es",
+    plugins: [wasm(), topLevelAwait()],
+  },
+
+  optimizeDeps: {
+    // This is necessary because otherwise `vite dev` includes two separate
+    // versions of the JS wrapper. This causes problems because the JS
+    // wrapper has a module level variable to track JS side heap
+    // allocations, and initializing this twice causes horrible breakage
+    exclude: [
+      "@automerge/automerge-wasm",
+      "@automerge/automerge-wasm/bundler/bindgen_bg.wasm",
+      "@syntect/wasm",
+    ],
+  },
+
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+})
