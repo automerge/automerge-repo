ce73a0051364a6702280089a03ef32892c942d96
Alex Currie-Clark <acurrieclark@gmail.com>
2023-06-02 16:59:18 +0100

    Fix missing `.js` extension

--- packages/automerge-repo/src/DocHandle.ts
+++ packages/automerge-repo/src/DocHandle.ts
@@ -17,7 +17,7 @@ import { waitFor } from "xstate/lib/waitFor.js"
 import { headsAreSame } from "./helpers/headsAreSame.js"
 import { pause } from "./helpers/pause.js"
 import type { ChannelId, DocumentId, PeerId } from "./types.js"
-import { withTimeout, TimeoutError } from "./helpers/withTimeout"
+import { withTimeout, TimeoutError } from "./helpers/withTimeout.js"
 
 /** DocHandle is a wrapper around a single Automerge document that lets us listen for changes. */
 export class DocHandle<T> //
