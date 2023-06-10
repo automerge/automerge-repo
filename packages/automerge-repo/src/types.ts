9f70aeeb392d3ced970e90c35f22faa019e8f138
HerbCaudill <herb@devresults.com>
2023-06-10 13:42:55 +0200

    types: add new Message types

--- packages/automerge-repo/src/types.ts
+++ packages/automerge-repo/src/types.ts
@@ -1,3 +1,22 @@
 export type DocumentId = string & { __documentId: true }
 export type PeerId = string & { __peerId: false }
 export type ChannelId = string & { __channelId: false }
+
+// Messages
+
+export type SyncMessage = {
+  type: "SYNC_MESSAGE"
+  senderId: PeerId
+  recipientId: PeerId
+  documentId: DocumentId
+  payload: Uint8Array // Automerge binary sync message
+}
+
+export type EphemeralMessage = {
+  type: "EPHEMERAL_MESSAGE"
+  senderId: PeerId
+  documentId: DocumentId
+  payload: Uint8Array // CBOR-encoded payload
+}
+
+export type Message = SyncMessage | EphemeralMessage
