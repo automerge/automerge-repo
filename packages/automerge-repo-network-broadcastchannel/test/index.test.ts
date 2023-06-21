6912ae506a39365dea0cc878a43dce58efc8d982
HerbCaudill <herb@devresults.com>
2023-06-21 20:21:02 +0100

    use acceptance tests

--- packages/automerge-repo-network-broadcastchannel/test/index.test.ts
+++ packages/automerge-repo-network-broadcastchannel/test/index.test.ts
@@ -1,33 +1,12 @@
-import { PeerId, Repo } from "@automerge/automerge-repo"
-import { eventPromise } from "@automerge/automerge-repo/src/helpers/eventPromise"
-import { assert } from "chai"
 import { BroadcastChannelNetworkAdapter } from "../src"
+import { SetupFn, runAdapterTests } from "@automerge/automerge-repo"
 
 describe("BroadcastChannel", () => {
-  it("can sync a document from one repo to another", async () => {
-    const aliceRepo = new Repo({
-      network: [new BroadcastChannelNetworkAdapter()],
-      peerId: "alice" as PeerId,
-    })
+  const setup: SetupFn = async () => {
+    const a = new BroadcastChannelNetworkAdapter()
+    const b = new BroadcastChannelNetworkAdapter()
+    return { adapters: [a, b] }
+  }
 
-    const charlieRepo = new Repo({
-      network: [new BroadcastChannelNetworkAdapter()],
-      peerId: "charlie" as PeerId,
-    })
-
-    const p = eventPromise(charlieRepo, "document")
-
-    const handle = aliceRepo.create<{ foo: string }>()
-    handle.change(d => {
-      d.foo = "bar"
-    })
-
-    await p
-
-    const charlieHandle = charlieRepo.find<{ foo: string }>(handle.documentId)
-    await eventPromise(charlieHandle, "change")
-    const v = await charlieHandle.value()
-
-    assert.equal(v.foo, "bar")
-  })
+  runAdapterTests(setup)
 })
