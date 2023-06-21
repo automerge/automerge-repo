4db45ba3df65be3ed5707ffa32bd307668bb8777
HerbCaudill <herb@devresults.com>
2023-06-21 20:13:52 +0100

    Network.test: add note

--- packages/automerge-repo/test/Network.test.ts
+++ packages/automerge-repo/test/Network.test.ts
@@ -1,8 +1,12 @@
 import assert from "assert"
 import { NetworkSubsystem } from "../src/network/NetworkSubsystem.js"
 
-describe("Network", () => {
-  it("TODO", () => {
+// Note: The sync tests in `Repo.test.ts` exercise the network subsystem, and the suite in
+// `automerge-repo-network-acceptance-tests` provides tests that individual adapters can
+// use to ensure that they work correctly.
+
+describe("Network subsystem", () => {
+  it("Can be instantiated", () => {
     const network = new NetworkSubsystem([])
     assert(network !== null)
   })
