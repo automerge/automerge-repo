7a553a542b5d1a5ff2ea878143212c0d32b526b1
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2023-06-25 16:01:00 -0700

    Add useDocumentId hook

--- packages/automerge-repo-react-hooks/src/index.ts
+++ packages/automerge-repo-react-hooks/src/index.ts
@@ -1,3 +1,4 @@
 export { useDocument } from "./useDocument.js"
+export { useDocumentId } from './useDocumentId.js'
 export { useHandle } from "./useHandle.js"
 export { RepoContext, useRepo } from "./useRepo.js"
