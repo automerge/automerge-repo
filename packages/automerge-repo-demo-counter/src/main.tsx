b97cc163d4fff4954876f91fd7ac2e7c5334fdc0
Peter van Hardenberg <pvh@pvh.ca>
2023-06-28 15:53:09 -0700

    instructions for debugging

--- packages/automerge-repo-demo-counter/src/main.tsx
+++ packages/automerge-repo-demo-counter/src/main.tsx
@@ -8,6 +8,14 @@ import { RepoContext } from "@automerge/automerge-repo-react-hooks"
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
 import { MessageChannelNetworkAdapter } from "@automerge/automerge-repo-network-messagechannel"
 
+// We run the network & storage in a separate file and the tabs themselves are stateless and lightweight.
+// This means we only ever create one websocket connection to the sync server, we only do our writes in one place
+// (no race conditions) and we get local real-time sync without the overhead of broadcast channel.
+// The downside is that to debug any problems with the sync server you'll need to find the shared-worker and inspect it.
+// In Chrome-derived browsers the URL is chrome://inspect/#workers. In Firefox it's about:debugging#workers.
+// In Safari it's Develop > Show Web Inspector > Storage > IndexedDB > automerge-repo-demo-counter.
+// Note that the non-Chrome instructions were autocompleted by Copilot and may be hallucinations. Good luck!
+
 // FIXME - had an issue with shared worker missing the connect message on the first startup
 // if it was also loading wasm - unsure what the issue is but repeating the sharedworker
 // in the only workaround we have at the moment
