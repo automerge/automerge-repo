edd4fc6f744cb3caa767c6df63329d4e6f79880a
Peter van Hardenberg <pvh@pvh.ca>
2023-06-28 15:49:01 -0700

    add shared-worker support for local sync

--- packages/automerge-repo-demo-counter/src/main.tsx
+++ packages/automerge-repo-demo-counter/src/main.tsx
@@ -6,14 +6,47 @@ import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-networ
 import { LocalForageStorageAdapter } from "@automerge/automerge-repo-storage-localforage"
 import { RepoContext } from "@automerge/automerge-repo-react-hooks"
 import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
+import { MessageChannelNetworkAdapter } from "@automerge/automerge-repo-network-messagechannel"
 
-const repo = new Repo({
-  network: [
-    new BroadcastChannelNetworkAdapter(),
-    new BrowserWebSocketClientAdapter("ws://localhost:3030"),
-  ],
-  storage: new LocalForageStorageAdapter(),
-})
+// FIXME - had an issue with shared worker missing the connect message on the first startup
+// if it was also loading wasm - unsure what the issue is but repeating the sharedworker
+// in the only workaround we have at the moment
+let sharedWorker = await createSharedWorker()
+function createSharedWorker(): Promise<SharedWorker> {
+  return new Promise(resolve => {
+    let interval = setInterval(() => {
+      let worker = new SharedWorker(
+        new URL("./shared-worker.ts", import.meta.url),
+        {
+          type: "module",
+          name: "automerge-repo-shared-worker",
+        }
+      )
+      worker.port.onmessage = e => {
+        if (e.data === "READY") {
+          clearInterval(interval)
+          resolve(worker)
+        }
+      }
+    }, 100)
+  })
+}
+
+function setupSharedWorkerAndRepo() {
+  const repoNetworkChannel = new MessageChannel()
+  sharedWorker.port.postMessage({ repoNetworkPort: repoNetworkChannel.port2 }, [
+    repoNetworkChannel.port2,
+  ])
+
+  const repo = new Repo({
+    network: [new MessageChannelNetworkAdapter(repoNetworkChannel.port1)],
+    sharePolicy: async peerId => peerId.includes("shared-worker"),
+  })
+
+  return repo
+}
+
+const repo = setupSharedWorkerAndRepo()
 
 let rootDocId = localStorage.rootDocId
 if (!rootDocId) {
