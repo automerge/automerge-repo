1335698359cf3cecc1359f60985233b4f3073bb6
Alex Currie-Clark <acurrieclark@gmail.com>
2023-09-11 17:35:32 +0100

    Allow broadcastchannel channel name to be configured

--- packages/automerge-repo-network-broadcastchannel/test/index.test.ts
+++ packages/automerge-repo-network-broadcastchannel/test/index.test.ts
@@ -3,6 +3,7 @@ import {
   type SetupFn,
   runAdapterTests,
 } from "../../automerge-repo/src/helpers/tests/network-adapter-tests.js"
+import { PeerId } from "@automerge/automerge-repo"
 
 describe("BroadcastChannel", () => {
   const setup: SetupFn = async () => {
@@ -14,4 +15,29 @@ describe("BroadcastChannel", () => {
   }
 
   runAdapterTests(setup)
+
+  it("should allow a channel name to be specified in the options", async () => {
+    const a = new BroadcastChannelNetworkAdapter({ channelName: "test" })
+    const b = new BroadcastChannelNetworkAdapter({ channelName: "test" })
+
+    // this adapter should never connect
+    const c = new BroadcastChannelNetworkAdapter()
+
+    const aConnect = new Promise<void>(resolve => {
+      a.once("peer-candidate", () => resolve())
+    })
+
+    const cShouldNotConnect = new Promise<void>((resolve, reject) => {
+      c.once("peer-candidate", () => reject("c should not connect"))
+
+      setTimeout(() => {
+        resolve()
+      }, 100)
+    })
+
+    a.connect("a" as PeerId)
+    b.connect("b" as PeerId)
+
+    return Promise.all([aConnect, cShouldNotConnect])
+  })
 })
