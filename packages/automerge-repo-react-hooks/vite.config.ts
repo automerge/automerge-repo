74dc8e84ad95f99009216ac9fa328564adb9a699
HerbCaudill <herb@devresults.com>
2023-10-05 20:53:20 +0200

    separate package-level vite config from monorepo vitest config

---
+++ packages/automerge-repo-react-hooks/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+import wasm from "vite-plugin-wasm"
+import topLevelAwait from "vite-plugin-top-level-await"
+
+export default defineConfig({
+  plugins: [react(), wasm(), topLevelAwait()],
+  worker: {
+    plugins: [wasm(), topLevelAwait()],
+  },
+})
