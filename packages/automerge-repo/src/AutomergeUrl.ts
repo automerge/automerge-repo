af2927e159da089533c50047ce516927231d1ba8
HerbCaudill <herb@devresults.com>
2023-10-16 17:21:48 +0200

    AutomergeUrl: comments etc

--- packages/automerge-repo/src/AutomergeUrl.ts
+++ packages/automerge-repo/src/AutomergeUrl.ts
@@ -8,30 +8,24 @@ import bs58check from "bs58check"
 
 export const urlPrefix = "automerge:"
 
-/**
- * given an Automerge URL, return a decoded DocumentId (and the encoded DocumentId)
- *
- * @param url
- * @returns { binaryDocumentId: BinaryDocumentId, documentId: DocumentId }
- */
+/** Given an Automerge URL, returns the DocumentId in both base58check-encoded form and binary form */
 export const parseAutomergeUrl = (url: AutomergeUrl) => {
   const { binaryDocumentId, documentId } = parts(url)
   if (!binaryDocumentId) throw new Error("Invalid document URL: " + url)
-  return { binaryDocumentId, documentId }
+  return {
+    /** decoded DocumentId */
+    binaryDocumentId,
+    /** encoded DocumentId */
+    documentId,
+  }
 }
 
 /**
- * Given a documentId in either canonical form, return an Automerge URL
+ * Given a documentId in either binary or base58check-encoded form, returns an Automerge URL.
  * Throws on invalid input.
- * Note: this is an object because we anticipate adding fields in the future.
- * @param { documentId: BinaryDocumentId | DocumentId }
- * @returns AutomergeUrl
+ * Note: This function takes an object because we anticipate adding fields in the future.
  */
-export const stringifyAutomergeUrl = ({
-  documentId,
-}: {
-  documentId: DocumentId | BinaryDocumentId
-}): AutomergeUrl => {
+export const stringifyAutomergeUrl = ({ documentId }: UrlOptions) => {
   if (documentId instanceof Uint8Array)
     return (urlPrefix +
       binaryToDocumentId(documentId as BinaryDocumentId)) as AutomergeUrl
@@ -42,10 +36,8 @@ export const stringifyAutomergeUrl = ({
 }
 
 /**
- * Given a string, return true if it is a valid Automerge URL
- * also acts as a type discriminator in Typescript.
- * @param str: URL candidate
- * @returns boolean
+ * Given a string, returns true if it is a valid Automerge URL. This function also acts as a type
+ * discriminator in Typescript.
  */
 export const isValidAutomergeUrl = (
   str: string | undefined | null
@@ -57,42 +49,43 @@ export const isValidAutomergeUrl = (
 }
 
 /**
- * generateAutomergeUrl produces a new AutomergeUrl.
- * generally only called by create(), but used in tests as well.
- * @returns a new Automerge URL with a random UUID documentId
+ * Returns a new Automerge URL with a random UUID documentId. Called by create(), and also used by tests.
  */
 export const generateAutomergeUrl = (): AutomergeUrl =>
   stringifyAutomergeUrl({
     documentId: Uuid.v4(null, new Uint8Array(16)) as BinaryDocumentId,
   })
 
-export const documentIdToBinary = (
-  docId: DocumentId
-): BinaryDocumentId | undefined =>
+export const documentIdToBinary = (docId: DocumentId) =>
   bs58check.decodeUnsafe(docId) as BinaryDocumentId | undefined
 
-export const binaryToDocumentId = (docId: BinaryDocumentId): DocumentId =>
+export const binaryToDocumentId = (docId: BinaryDocumentId) =>
   bs58check.encode(docId) as DocumentId
 
-export const parseLegacyUUID = (str: string): AutomergeUrl | undefined => {
-  if (Uuid.validate(str)) {
-    const uuid = Uuid.parse(str) as BinaryDocumentId
-    return stringifyAutomergeUrl({ documentId: uuid })
-  }
-  return undefined
+export const parseLegacyUUID = (str: string) => {
+  if (!Uuid.validate(str)) return undefined
+  const uuid = Uuid.parse(str) as BinaryDocumentId
+  return stringifyAutomergeUrl({ documentId: uuid })
 }
 
 /**
  * parts breaks up the URL into constituent pieces,
  * eventually this could include things like heads, so we use this structure
  * we return both a binary & string-encoded version of the document ID
- * @param str
- * @returns { binaryDocumentId, documentId }
  */
 const parts = (str: string) => {
   const regex = new RegExp(`^${urlPrefix}(\\w+)$`)
   const [_, docMatch] = str.match(regex) || []
   const documentId = docMatch as DocumentId
   const binaryDocumentId = documentIdToBinary(documentId)
-  return { binaryDocumentId, documentId }
+  return {
+    /** decoded DocumentId */
+    binaryDocumentId,
+    /** encoded DocumentId */
+    documentId,
+  }
+}
+
+type UrlOptions = {
+  documentId: DocumentId | BinaryDocumentId
 }
