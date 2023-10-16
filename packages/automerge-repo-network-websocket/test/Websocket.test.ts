ceae9d46d84e4bf2ebe4604872f5f25fcfd30c83
Alex Good <alex@memoryandthought.me>
2023-10-16 16:59:28 +0100

    automerge-repo-network-websocket: actually cleanup event handlers
    Add a test to avoid going through all this yet again.

--- packages/automerge-repo-network-websocket/test/Websocket.test.ts
+++ packages/automerge-repo-network-websocket/test/Websocket.test.ts
@@ -105,6 +105,37 @@ describe("The NodeWSServerAdapter", () => {
       selectedProtocolVersion: "1",
     })
   })
+  
+  it("should correctly clear event handlers on reconnect", async () => {
+    // This reproduces an issue where the BrowserWebSocketClientAdapter.connect
+    // call registered event handlers on the websocket but didn't clear them
+    // up again on a second call to connect. This combined with the reconnect
+    // timer to produce the following sequence of events:
+    //
+    // * first call to connect creates a socket and registers an "open"
+    //   handler. The "open" handler will attempt to send a join message
+    // * The connection is slow, so the reconnect timer fires before "open",
+    //   the reconnect timer calls connect again. this.socket is now a new socket
+    // * The other end replies to the first socket, "open"
+    // * The original "open" handler attempts to send a message, but on the new
+    //   socket (because it uses this.socket), which isn't open yet, so we get an
+    //   error that the socket is not ready
+    const { socket, server } = await startServer(0)
+    let port = (server.address()!! as AddressInfo).port
+    const serverUrl = `ws://localhost:${port}`
+    const serverAdapter = new NodeWSServerAdapter(socket)
+    const browserAdapter = new BrowserWebSocketClientAdapter(serverUrl)
+
+    const peerId = "testclient" as PeerId
+    browserAdapter.connect(peerId)
+    // simulate the reconnect timer firing before the other end has responded
+    // (which works here because we haven't yielded to the event loop yet so
+    // the server, which is on the same event loop as us, can't respond)
+    browserAdapter.connect(peerId)
+    // Now await, so the server responds on the first socket, if the listeners
+    // are cleaned up correctly we shouldn't throw
+    await pause(1)
+  })
 })
 
 async function serverHelloGivenClientHello(
@@ -144,3 +175,6 @@ async function firstMessage(
     })
   })
 }
+
+export const pause = (t = 0) =>
+  new Promise<void>(resolve => setTimeout(() => resolve(), t))
