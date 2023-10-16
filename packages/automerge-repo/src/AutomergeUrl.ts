f035a5c16d7a0869a313fada1135a01d30da1e21
HerbCaudill <herb@devresults.com>
2023-10-16 17:48:06 +0200

    AutomergeUrl: fold `parts` into `parseAutomergeUrl`

--- packages/automerge-repo/src/AutomergeUrl.ts
+++ packages/automerge-repo/src/AutomergeUrl.ts
@@ -10,10 +10,14 @@ export const urlPrefix = "automerge:"
 
 /** Given an Automerge URL, returns the DocumentId in both base58check-encoded form and binary form */
 export const parseAutomergeUrl = (url: AutomergeUrl) => {
-  const { binaryDocumentId, documentId } = parts(url)
+  const regex = new RegExp(`^${urlPrefix}(\\w+)$`)
+  const [_, docMatch] = url.match(regex) || []
+  const documentId = docMatch as DocumentId
+  const binaryDocumentId = documentIdToBinary(documentId)
+
   if (!binaryDocumentId) throw new Error("Invalid document URL: " + url)
   return {
-    /** decoded DocumentId */
+    /** unencoded DocumentId */
     binaryDocumentId,
     /** encoded DocumentId */
     documentId,
@@ -55,9 +59,13 @@ export const isValidAutomergeUrl = (
   str: string | undefined | null
 ): str is AutomergeUrl => {
   if (!str || !str.startsWith(urlPrefix)) return false
-
-  const { binaryDocumentId: documentId } = parts(str)
-  return documentId ? true : false
+  const automergeUrl = str as AutomergeUrl
+  try {
+    parseAutomergeUrl(automergeUrl)
+    return true
+  } catch {
+    return false
+  }
 }
 
 /**
@@ -76,26 +84,8 @@ export const binaryToDocumentId = (docId: BinaryDocumentId) =>
 
 export const parseLegacyUUID = (str: string) => {
   if (!Uuid.validate(str)) return undefined
-  const uuid = Uuid.parse(str) as BinaryDocumentId
-  return stringifyAutomergeUrl({ documentId: uuid })
-}
-
-/**
- * parts breaks up the URL into constituent pieces,
- * eventually this could include things like heads, so we use this structure
- * we return both a binary & string-encoded version of the document ID
- */
-const parts = (str: string) => {
-  const regex = new RegExp(`^${urlPrefix}(\\w+)$`)
-  const [_, docMatch] = str.match(regex) || []
-  const documentId = docMatch as DocumentId
-  const binaryDocumentId = documentIdToBinary(documentId)
-  return {
-    /** decoded DocumentId */
-    binaryDocumentId,
-    /** encoded DocumentId */
-    documentId,
-  }
+  const documentId = Uuid.parse(str) as BinaryDocumentId
+  return stringifyAutomergeUrl({ documentId })
 }
 
 type UrlOptions = {
