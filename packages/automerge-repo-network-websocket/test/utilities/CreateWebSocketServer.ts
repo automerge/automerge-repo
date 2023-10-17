7c02f5afe6a8b0e09a9f3a59bd7e8735b5fa5a03
HerbCaudill <herb@devresults.com>
2023-10-17 18:14:59 +0200

    remove unused helpers

--- packages/automerge-repo-network-websocket/test/utilities/CreateWebSocketServer.ts
+++
@@ -1,8 +0,0 @@
-import http from "http"
-import WebSocket from "ws"
-
-function createWebSocketServer(server: http.Server) {
-  return new WebSocket.Server({ server })
-}
-
-export { createWebSocketServer }
