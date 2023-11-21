ec3d0f1bef33b1995ceaadbb17499e65211da32c
Frederico Rodrigues <fred@devresults.com>
2023-11-21 14:03:10 -0500

    LocalFirstAuthSyncServer: Adding usage for setup
    And renaming should add a team test to Alice can create a team

--- packages/automerge-repo-auth-localfirst-syncserver/test/LocalFirstAuthSyncServer.test.ts
+++ packages/automerge-repo-auth-localfirst-syncserver/test/LocalFirstAuthSyncServer.test.ts
@@ -1,32 +1,13 @@
-import { PeerId, Repo } from "@automerge/automerge-repo"
-import { LocalFirstAuthProvider } from "@automerge/automerge-repo-auth-localfirst"
 import { eventPromise } from "@automerge/automerge-repo-auth-localfirst/test/helpers/eventPromise"
-import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket"
-import { Team, createDevice, createKeyset, createUser } from "@localfirst/auth"
-import fs from "fs"
-import os from "os"
-import path from "path"
-import { getPortPromise as getAvailablePort } from "portfinder"
+import { Team, createTeam, loadTeam, device } from "@localfirst/auth"
+
 import { describe, expect, it } from "vitest"
 import { LocalFirstAuthSyncServer } from "../src/index.js"
-import { NodeFSStorageAdapter } from "@automerge/automerge-repo-storage-nodefs"
-
-const storageDir = fs.mkdtempSync(
-  path.join(os.tmpdir(), "automerge-repo-tests")
-)
-const host = "localhost"
+import { host, setup } from "./helpers/setup.js"
 
 describe("LocalFirstAuthSyncServer", () => {
   let url: string
   let server: LocalFirstAuthSyncServer
-
-  const setup = async () => {
-    const port = await getAvailablePort({ port: 3100 })
-    url = `localhost:${port}`
-    server = new LocalFirstAuthSyncServer(host)
-    await server.listen({ port, silent: true, storageDir })
-    return { url, server }
-  }
 
   it("should start a server", async () => {
     const { url } = await setup()
@@ -49,33 +30,15 @@ describe("LocalFirstAuthSyncServer", () => {
     expect(server.publicKeys).toEqual(keys)
   })
 
-  it("should add a team", async () => {
-    const { url, server } = await setup()
-
-    const user = createUser("alice")
-    const device = createDevice(user.userId, "laptop")
-
-    // add the team to a new auth provider
-    const storage = new NodeFSStorageAdapter(storageDir)
-    const auth = new LocalFirstAuthProvider({ user, device, storage })
-
-    const adapter = auth.wrap(new BrowserWebSocketClientAdapter(`ws://${url}`))
-
-    // set up our repo
-    const repo = new Repo({
-      peerId: device.deviceId as PeerId,
-      network: [adapter],
-      storage,
-    })
+  it("Alice can create a team", async () => {
+    const { users, url, server } = await setup(["alice"])
+    const { alice } = users
 
     // create a team
-    const teamKeys = createKeyset({ type: "TEAM", name: "TEAM" })
-    const team = new Team({
-      teamName: "team A",
-      context: { user, device },
-      teamKeys,
-    })
-    auth.addTeam(team)
+    const teamContext = { user: alice.user, device: alice.device }
+
+    const team = createTeam("team A", teamContext)
+    alice.authProvider.addTeam(team)
 
     // get the server's public keys
     const response = await fetch(`http://${url}/keys`)
@@ -96,7 +59,7 @@ describe("LocalFirstAuthSyncServer", () => {
     })
 
     // when we're authenticated, we get a peer event
-    const { peerId } = await eventPromise(repo.networkSubsystem, "peer")
+    const { peerId } = await eventPromise(alice.repo.networkSubsystem, "peer")
     expect(peerId).toEqual(host)
   })
 })
