5ab8fe223b49a1db063b7e024c7450f385c81999
Frederico Rodrigues <fred@devresults.com>
2023-11-21 14:04:09 -0500

    LocalFirstAuthSynServer: Adding new tests for checking communication
    Checking that changes done on aliceTeam are also seen by bobTeam

--- packages/automerge-repo-auth-localfirst-syncserver/test/LocalFirstAuthSyncServer.test.ts
+++ packages/automerge-repo-auth-localfirst-syncserver/test/LocalFirstAuthSyncServer.test.ts
@@ -62,6 +62,57 @@ describe("LocalFirstAuthSyncServer", () => {
     const { peerId } = await eventPromise(alice.repo.networkSubsystem, "peer")
     expect(peerId).toEqual(host)
   })
+
+  it("Allows two users to communicate", async () => {
+    const { users, url, server } = await setup(["alice", "bob"])
+    const { alice, bob } = users
+
+    // create a team
+    const teamContext = { user: alice.user, device: alice.device }
+
+    const aliceTeam: Team = createTeam("team A", teamContext)
+    alice.authProvider.addTeam(aliceTeam)
+    aliceTeam.addForTesting(bob.user, [], device.redactDevice(bob.device))
+
+    // get the server's public keys
+    const response = await fetch(`http://${url}/keys`)
+    const keys = await response.json()
+
+    // add the server's public keys to the team
+    aliceTeam.addServer({ host, keys })
+
+    // register the team with the server
+    // (we don't await this because otherwise peer will fire before we're listening)
+    void fetch(`http://${url}/teams`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        serializedGraph: aliceTeam.save(),
+        teamKeyring: aliceTeam.teamKeyring(),
+      }),
+    })
+
+    // when we're authenticated, we get a peer event
+    await eventPromise(alice.repo.networkSubsystem, "peer")
+
+    // We are making sure that bob is on the same team as alice and the server
+    const bobTeam = loadTeam(
+      aliceTeam.graph,
+      { user: bob.user, device: bob.device },
+      aliceTeam.teamKeyring()
+    )
+    await bob.authProvider.addTeam(bobTeam)
+
+    await eventPromise(bob.repo.networkSubsystem, "peer")
+
+    //Now we are going to test that Bob is going to see alice's changes to the team
+    aliceTeam.addRole("MANAGERS")
+
+    // We need to wait Bob team to get updated so we can check that both teams are in sync
+    await eventPromise(bobTeam, "updated")
+
+    expect(bobTeam.hasRole("MANAGERS")).toBe(true)
+  })
 })
 
 const lookLikeServerKeys = (maybeKeyset: any) =>
