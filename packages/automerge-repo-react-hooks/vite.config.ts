d2ec026552bba09c86129bbe853b2aa7d024c3a7
Ryan Kois <ryan.kois@gmail.com>
2023-12-02 20:54:53 -0800

    build hooks package with vite and stop bundling react

--- packages/automerge-repo-react-hooks/vite.config.ts
+++ packages/automerge-repo-react-hooks/vite.config.ts
@@ -1,11 +1,39 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import { resolve } from "path"
+import dts from "vite-plugin-dts"
 import wasm from "vite-plugin-wasm"
 import topLevelAwait from "vite-plugin-top-level-await"
+import { visualizer } from "rollup-plugin-visualizer"
 
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    process.env.VISUALIZE && visualizer(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          'react-dom': 'ReactDOM',
+        }
+      }
+    },
+  },
   worker: {
     plugins: [wasm(), topLevelAwait()],
-  },
+  }
 })
