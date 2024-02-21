b591ab9f3c4010e475a7aac4990f49c0209ff4ac
Robin Bühler <git+r@obin.ch>
2024-02-21 13:13:42 +0000

    chore: fix lint errors

--- packages/automerge-repo-storage-opfs/src/index.ts
+++ packages/automerge-repo-storage-opfs/src/index.ts
@@ -4,91 +4,91 @@
  * @packageDocumentation
  */
 
-import { Chunk, StorageAdapter, StorageKey } from '@automerge/automerge-repo';
+import { Chunk, StorageAdapter, StorageKey } from '@automerge/automerge-repo'
 
 export class OPFSStorageAdapter extends StorageAdapter {
-  private baseDirectory: string;
-  private baseDirectoryHandle: FileSystemDirectoryHandle;
+  private baseDirectory: string
+  private baseDirectoryHandle: FileSystemDirectoryHandle
 
   constructor(baseDirectory: string = "automerge-repo-data") {
-    super();
-    this.baseDirectory = baseDirectory;
+    super()
+    this.baseDirectory = baseDirectory
   }
 
   async load(key: StorageKey): Promise<Uint8Array | undefined> {
-    const fileName = getKey(key);
-    await this.initialize();
+    const fileName = getKey(key)
+    await this.initialize()
 
     try {
-      const fileHandle = await this.baseDirectoryHandle.getFileHandle(fileName);
-      const file = await fileHandle.getFile();
-      const arrayBuffer = await file.arrayBuffer();
-      return new Uint8Array(arrayBuffer);
+      const fileHandle = await this.baseDirectoryHandle.getFileHandle(fileName)
+      const file = await fileHandle.getFile()
+      const arrayBuffer = await file.arrayBuffer()
+      return new Uint8Array(arrayBuffer)
     } catch (error) {
       // don't throw if file not found
       if (error.name === "NotFoundError") return undefined
-      throw error;
+      throw error
     }
   }
 
   async save(key: StorageKey, data: Uint8Array): Promise<void> {
-    const fileName = getKey(key);
-    await this.initialize();
+    const fileName = getKey(key)
+    await this.initialize()
 
-    const fileHandle = await this.baseDirectoryHandle.getFileHandle(fileName, { create: true });
-    const fileWritable = await fileHandle.createWritable();
-    await fileWritable.write(data);
-    return fileWritable.close();
+    const fileHandle = await this.baseDirectoryHandle.getFileHandle(fileName, { create: true })
+    const fileWritable = await fileHandle.createWritable()
+    await fileWritable.write(data)
+    return fileWritable.close()
   }
 
   async remove(key: StorageKey): Promise<void> {
-    const fileName = getKey(key);
-    await this.initialize();
+    const fileName = getKey(key)
+    await this.initialize()
 
     try {
-      return this.baseDirectoryHandle.removeEntry(fileName);
+      return this.baseDirectoryHandle.removeEntry(fileName)
     } catch (error) {
       // don't throw if file not found
       if (error.name === "NotFoundError") return undefined
-      throw error;
+      throw error
     }
   }
 
   async loadRange(keyPrefix: StorageKey): Promise<Chunk[]> {
-    const chunks: Chunk[] = [];
-    const prefix = getKey(keyPrefix);
-    await this.initialize();
+    const chunks: Chunk[] = []
+    const prefix = getKey(keyPrefix)
+    await this.initialize()
 
     for await (const [name] of this.baseDirectoryHandle) {
         if (name.startsWith(prefix)) {
-            const fileHandle = await this.baseDirectoryHandle.getFileHandle(name);
-            const file = await fileHandle.getFile();
-            const arrayBuffer = await file.arrayBuffer();
-            chunks.push({key: name.split("_"), data: new Uint8Array(arrayBuffer)});
+            const fileHandle = await this.baseDirectoryHandle.getFileHandle(name)
+            const file = await fileHandle.getFile()
+            const arrayBuffer = await file.arrayBuffer()
+            chunks.push({key: name.split("_"), data: new Uint8Array(arrayBuffer)})
         }
     }
 
-    return chunks;
+    return chunks
   }
 
   async removeRange(keyPrefix: StorageKey): Promise<void> {
-    const prefix = getKey(keyPrefix);
-    await this.initialize();
+    const prefix = getKey(keyPrefix)
+    await this.initialize()
 
     for await (const [name] of this.baseDirectoryHandle) {
         if (name.startsWith(prefix)) {
-            await this.baseDirectoryHandle.removeEntry(name);
+            await this.baseDirectoryHandle.removeEntry(name)
         }
     }
   }
 
   private async initialize() {
     if(!this.baseDirectoryHandle) {
-      const root = await navigator.storage.getDirectory();
-      this.baseDirectoryHandle = await root.getDirectoryHandle(this.baseDirectory, { create: true });
+      const root = await navigator.storage.getDirectory()
+      this.baseDirectoryHandle = await root.getDirectoryHandle(this.baseDirectory, { create: true })
     }
   }
 
 }
 
-const getKey = (key: StorageKey): string => key.join("_");
+const getKey = (key: StorageKey): string => key.join("_")
