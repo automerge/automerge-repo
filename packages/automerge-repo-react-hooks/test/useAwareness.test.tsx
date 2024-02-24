85e391b4e6b1dcb233ee276a666ff76de4846a9e
Mauve Signweaver <contact@mauve.moe>
2024-02-23 19:00:06 -0500

    initial tests for useAwareness react hooks

--- packages/automerge-repo-react-hooks/test/useAwareness.test.tsx
+++ packages/automerge-repo-react-hooks/test/useAwareness.test.tsx
@@ -1,19 +1,140 @@
-import { renderHook } from "@testing-library/react"
-import { describe, expect, test, vi } from "vitest"
-import { RepoContext, useRepo } from "../src/useRepo.js"
-import { Repo } from "@automerge/automerge-repo"
-import React from "react"
+import { renderHook, act, waitFor } from "@testing-library/react"
+import { describe, expect, test } from "vitest"
+import { MessageChannelNetworkAdapter } from "../../automerge-repo-network-messagechannel/src/index.js"
+import { useLocalAwareness } from "../src/useLocalAwareness.js"
+import { useRemoteAwareness } from "../src/useRemoteAwareness.js"
+import { Repo, PeerId } from "@automerge/automerge-repo"
+
+function setupMesh(n = 2) {
+  const abChannel = new MessageChannel()
+
+  const { port1: ab, port2: ba } = abChannel
+
+  const alice = "alice" as PeerId
+  const aliceRepo = new Repo({
+    network: [new MessageChannelNetworkAdapter(ab)],
+    peerId: alice,
+  })
+
+  const bob = "bob" as PeerId
+  const bobRepo = new Repo({
+    network: [new MessageChannelNetworkAdapter(ba)],
+    peerId: bob,
+  })
+
+  return { alice, bob, aliceRepo, bobRepo }
+}
 
 describe("use{Local,Remote}Awareness", () => {
   test("it should allow a user to create a local awareness", () => {
     const repo = new Repo({ network: [] })
-    // Prevent console spam by swallowing console.error "uncaught error" message
-    const spy = vi.spyOn(console, "error")
-    spy.mockImplementation(() => {})
-    expect(() => renderHook(() => useRepo())).toThrow(
-      /Repo was not found on RepoContext/
+    const handle = repo.create("Hello World")
+
+    const { result, rerender } = renderHook(() =>
+      useLocalAwareness({
+        handle,
+        userId: "alice",
+        initialState: "Hello World",
+      })
     )
-    spy.mockRestore()
+
+    const [state, setState] = result.current
+
+    expect(state).toEqual("Hello World")
+
+    act(() => {
+      setState("Goodbye")
+    })
+
+    rerender()
+
+    const [state2, setState2] = result.current
+
+    expect(state2).toEqual("Goodbye")
+  })
+
+  test("it should show state on remote awareness for same doc handle", () => {
+    const userId = "alice"
+    const initialState = "Hello World"
+
+    const repo = new Repo({ network: [] })
+    const handle = repo.create("Hello World")
+
+    const { result, rerender } = renderHook(() => [
+      useLocalAwareness({
+        handle,
+        userId,
+        initialState,
+      }),
+      useRemoteAwareness({
+        handle,
+        localUserId: userId,
+      }),
+    ])
+
+    const [local, remote] = result.current
+
+    const [peerStates, heartbeats] = remote
+
+    // For just the local peer it should ignore itself
+    expect(peerStates).toEqual({})
+    expect(heartbeats).toEqual({})
+  })
+
+  test("it should send local state from alice to remote state of bob", async () => {
+    let timeCount = 0
+    function getTime() {
+      return timeCount++
+    }
+
+    const initialState = "Hello World"
+    const { alice, bob, aliceRepo, bobRepo } = setupMesh()
+
+    const aliceHandle = aliceRepo.create("Hello World")
+    const bobHandle = bobRepo.find(aliceHandle.url)
+
+    const { result: resultA, rerender: rerenderA } = renderHook(() => [
+      useLocalAwareness({
+        handle: aliceHandle,
+        userId: alice,
+        initialState,
+      }),
+      useRemoteAwareness({
+        handle: aliceHandle,
+        localUserId: alice,
+        getTime,
+      }),
+    ])
+
+    // TODO: Need to wait for initialization somehow?
+    await new Promise(resolve => setTimeout(resolve, 100))
+
+    const { result: resultB, rerender: rerenderB } = renderHook(() => [
+      useLocalAwareness({
+        handle: bobHandle,
+        userId: bob,
+        initialState,
+      }),
+      useRemoteAwareness({
+        handle: bobHandle,
+        localUserId: bob,
+        getTime,
+      }),
+    ])
+
+    await waitFor(() => {
+      rerenderA()
+      rerenderB()
+      const [localB, remoteB] = resultB.current
+
+      const [peerStates, heartbeats] = remoteB
+
+      // For just the local peer it should ignore itself
+      expect(peerStates).toEqual({ [alice]: initialState })
+      // expect two heartbeats from alice
+      // their inital, then second once seeing bob
+      expect(heartbeats).toEqual({ [alice]: 2 })
+    })
   })
 })
 
