0fb2cfc52a98f311b6896ca104303dfdd2e82905
Bijela Gora <bijela_gora@protonmail.com>
2024-03-08 21:13:57 +0300

    test: use node env to run NodeFSStorageAdapter adapter

---
+++ packages/automerge-repo-storage-nodefs/vitest.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig, mergeConfig } from "vitest/config"
+import rootConfig from '../../vitest.config'
+
+export default mergeConfig(rootConfig, defineConfig({
+  test: {
+    environment: "node"
+  },
+}))
