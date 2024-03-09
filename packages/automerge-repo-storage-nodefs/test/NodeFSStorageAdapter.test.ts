00a4780a6f7ebb4af1725b2232afad7275d59632
Bijela Gora <bijela_gora@protonmail.com>
2024-03-09 23:33:58 +0300

    chore: remove unused import

--- packages/automerge-repo-storage-nodefs/test/NodeFSStorageAdapter.test.ts
+++ packages/automerge-repo-storage-nodefs/test/NodeFSStorageAdapter.test.ts
@@ -1,7 +1,6 @@
 import * as fs from "node:fs"
 import * as os from "node:os"
 import * as path from "node:path"
-import * as crypto from "node:crypto"
 import { afterAll, beforeEach, describe, expect, it } from "vitest"
 import { runStorageAdapterTests } from "@automerge/automerge-repo/src/helpers/tests/storage-adapter-tests"
 import { NodeFSStorageAdapter } from "../src"
