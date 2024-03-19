8c66f0920489abf63e222e4f96bf2d1300e44a0f
alexjg <alex@memoryandthought.me>
2024-03-19 14:53:16 +0000

    create-vite-app: update to match quickstart (#315)

--- packages/create-vite-app/template/src/App.tsx
+++ packages/create-vite-app/template/src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import automergeLogo from "./assets/automerge.png"
 import "./App.css"
 import { AutomergeUrl } from "@automerge/automerge-repo"
@@ -16,27 +14,21 @@ function App({ docUrl }: { docUrl: AutomergeUrl }) {
   return (
     <>
       <div>
-        <a href="automerge.org" target="_blank">
-          <img src={automergeLogo} className="logo" alt="Automerge logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://automerge.org" target="_blank">
+          <img src={automergeLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Automerge + Vite + React</h1>
+      <h1>Meet Automerge</h1>
       <div className="card">
-        <button onClick={() => changeDoc(d => d.counter.increment(1))}>
+        <button onClick={() => changeDoc((d) => d.counter.increment(1))}>
           count is {doc && doc.counter.value}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Open this page in another tab to watch the updates synchronize
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Built with Automerge, Vite, React, and TypeScript
       </p>
     </>
   )
--- packages/create-vite-app/template/vite.config.ts
+++ packages/create-vite-app/template/vite.config.ts
@@ -3,7 +3,18 @@ import react from "@vitejs/plugin-react"
 import wasm from "vite-plugin-wasm"
 import topLevelAwait from "vite-plugin-top-level-await"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait(), react()],
+  // customize this to your repo name for github pages deploy
+  base: "/",
+
+  build: {
+    target: "esnext",
+  },
+
+  plugins: [wasm(), react()],
+
+  worker: {
+    format: "es",
+    plugins: () => [wasm()],
+  },
 })
