16cc61043b226682256d3ba456b2a55ff4ac9223
yarolegovich <yarolegovich@gmail.com>
2024-08-01 19:18:28 +0300

    added failing tests

--- packages/automerge-repo/src/helpers/DummyNetworkAdapter.ts
+++ packages/automerge-repo/src/helpers/DummyNetworkAdapter.ts
@@ -5,10 +5,21 @@ export class DummyNetworkAdapter extends NetworkAdapter {
   #sendMessage?: SendMessageFn
 
   #ready = false
+  #isDroppingMessages = false;
   #readyResolver?: () => void
   #readyPromise: Promise<void> = new Promise<void>(resolve => {
     this.#readyResolver = resolve
   })
+
+  #droppedMessages: Message[] = [];
+
+  dropMessages(drop: boolean) {
+    this.#isDroppingMessages = drop;
+  }
+
+  getDroppedMessages() {
+    return [...this.#droppedMessages];
+  }
 
   isReady() {
     return this.#ready
@@ -49,11 +60,19 @@ export class DummyNetworkAdapter extends NetworkAdapter {
   }
 
   override send(message: Message) {
-    this.#sendMessage?.(message)
+    if (this.#isDroppingMessages) {
+      this.#droppedMessages.push(message);
+    } else {
+      this.#sendMessage?.(message)
+    }
   }
 
   receive(message: Message) {
-    this.emit("message", message)
+    if (this.#isDroppingMessages) {
+      this.#droppedMessages.push(message);
+    } else {
+      this.emit("message", message)
+    }
   }
 
   static createConnectedPair({ latency = 10 }: { latency?: number } = {}) {
