91abc43c75e10e7ad5e10524bec744621fd23ba6
Grégoire Geis <opensource@gregoirege.is>
2025-07-24 18:05:01 +0900

    Fix document saves made within 100ms being discarded

--- packages/automerge-repo/src/helpers/throttle.ts
+++ packages/automerge-repo/src/helpers/throttle.ts
@@ -41,3 +41,44 @@ export const throttle = <F extends (...args: Parameters<F>) => ReturnType<F>>(
     }, wait)
   }
 }
+
+/**
+ * Same as {@link throttle()}, but throttles calls separately based on a
+ * (weakly-referenced) key.
+ *
+ * Given two distinct objects `a` and `b`, and
+ * `f = throttleByKey(fn, (v) => v.k, 100)`, then the following sequence of
+ * calls:
+ *
+ * - `f({ k: a, v: 1 })`
+ * - `f({ k: a, v: 2 })`
+ * - `f({ k: b, v: 3 })`
+ *
+ * Will result in `fn({ k: a, v: 2 })` and `fn({ k: b, v: 3 }) being called
+ * after 100ms. Contrast this with {@link throttle()}, which would only call
+ * `fn({ k: b, v: 3 })` after 100ms.
+ */
+export const throttleByKey = <Arg, K extends WeakKey, F extends (arg: Arg) => ReturnType<F>>(
+  fn: F,
+  key: (arg: Arg) => K,
+  delay: number
+) => {
+  const callTimeouts = new WeakMap<K, /*lastArg=*/Arg>()
+
+  return function (arg: Arg) {
+    const k = key(arg)
+
+    if (!callTimeouts.has(k)) {
+      // No timeout yet, set it up.
+      setTimeout(() => {
+        const lastArg = callTimeouts.get(k)
+        if (lastArg !== undefined) {
+          callTimeouts.delete(k)
+          fn(lastArg)
+        }
+      }, delay)
+    }
+
+    callTimeouts.set(k, arg)
+  }
+}
