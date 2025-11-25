782a1cd629124fb009b0f67b759b4e8498b44ef4
Maciek Sakrejda <m.sakrejda@gmail.com>
2025-11-24 17:05:14 -0800

    Doc updates

--- packages/automerge-repo-react-hooks/src/usePresence.ts
+++ packages/automerge-repo-react-hooks/src/usePresence.ts
@@ -8,18 +8,40 @@ import {
 } from "@automerge/automerge-repo/slim"
 import { useInvalidate } from "./helpers/useInvalidate.js"
 
+export type UsePresenceOpts = Omit<PresenceOpts, "skipAutoInit">
+
 export type UsePresenceResult<State, Channel extends keyof State> = {
   peerStates: PeerPresenceView<State>
   localState: State
   update: (channel: Channel, value: State[Channel]) => void
 }
 
+/**
+ * This hook encapsulates ephemeral state communication for a specific doc
+ * handle. It tracks caller-provided local state and broadcasts that state to
+ * all peers. It sends periodic heartbeats when there are no state updates.
+ *
+ * It also tracks ephemeral state broadcast by peers and forces a re-render when
+ * the state of a peer changes.
+ *
+ * It cleans up (stops sending heartbeats and processing ephemeral messages)
+ * when it unmounts.
+ *
+ * It is implemented using {@link Presence}.
+ *
+ * @param handle - doc handle to use
+ * @param userId - our user id (this is unverified; peers can send anything)
+ * @param deviceId - our device id (like userId, this is unverified)
+ * @param initialState - the full initial state to broadcast to peers (updates do not trigger a re-render of the hook)
+ * @param opts - see {@link UsePresenceOpts} (updates do not trigger a re-render of the hook)
+ * @returns see {@link UsePresenceResult}
+ */
 export function usePresence<State, Channel extends keyof State>(
   handle: DocHandle<unknown>,
   userId: string,
   deviceId: string,
   initialState: State,
-  opts?: Omit<PresenceOpts, "skipAutoInit">
+  opts?: UsePresenceOpts
 ): UsePresenceResult<State, Channel> {
   const invalidate = useInvalidate()
   // Don't re-render based on changes to these: they are not expected to
