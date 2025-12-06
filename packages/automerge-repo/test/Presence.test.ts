596e70b39a58f2a6f5f6481ccbf792d1330a019f
Maciek Sakrejda <m.sakrejda@gmail.com>
2025-12-05 16:10:12 -0800

    Refactor tests

--- packages/automerge-repo/test/Presence.test.ts
+++ packages/automerge-repo/test/Presence.test.ts
@@ -1,18 +1,14 @@
-import { afterEach, beforeEach, describe, expect, it, vi } from "vitest"
+import { describe, expect, it } from "vitest"
 
-import {
-  Presence,
-  DEFAULT_HEARTBEAT_INTERVAL_MS,
-  PresenceEventHeartbeat,
-} from "../src/Presence.js"
+import { Presence, PresenceEventHeartbeat } from "../src/Presence.js"
 import { Repo } from "../src/Repo.js"
 import { PeerId } from "../src/types.js"
 import { DummyNetworkAdapter } from "../src/helpers/DummyNetworkAdapter.js"
 import { waitFor } from "./helpers/waitFor.js"
 
-describe("Presence", () => {
-  type PresenceState = { position: number }
+type PresenceState = { position: number }
 
+describe("Presence", () => {
   async function setup() {
     const alice = new Repo({ peerId: "alice" as PeerId })
     const bob = new Repo({ peerId: "bob" as PeerId })
@@ -25,46 +21,61 @@ describe("Presence", () => {
       alice.networkSubsystem.whenReady(),
       bob.networkSubsystem.whenReady(),
     ])
-    return [alice, bob]
+
+    const aliceHandle = alice.create({
+      test: "doc",
+    })
+    const alicePresence = new Presence<PresenceState>({
+      handle: aliceHandle,
+      userId: "alice",
+      deviceId: "phone",
+    })
+
+    const bobHandle = await bob.find(aliceHandle.url)
+    const bobPresence = new Presence<PresenceState>({
+      handle: bobHandle,
+      userId: "bob",
+      deviceId: "phone",
+    })
+
+    return {
+      alice: {
+        repo: alice,
+        handle: aliceHandle,
+        presence: alicePresence,
+      },
+      bob: {
+        repo: bob,
+        handle: bobHandle,
+        presence: bobPresence,
+      },
+    }
   }
 
-  describe("constructor", () => {
+  describe("start", () => {
     it("shares initial state", async () => {
-      const [alice, bob] = await setup()
-      const aliceHandle = alice.create({
-        test: "doc",
-      })
-      const alicePresence = new Presence<PresenceState>({
-        handle: aliceHandle,
-        userId: "alice",
-        deviceId: "phone",
-      })
-      alicePresence.start({
+      const { alice, bob } = await setup()
+
+      alice.presence.start({
         initialState: {
           position: 123,
         },
       })
 
-      const bobHandle = await bob.find(aliceHandle.url)
-      const bobPresence = new Presence<PresenceState>({
-        handle: bobHandle,
-        userId: "bob",
-        deviceId: "phone",
-      })
-      bobPresence.start({
+      bob.presence.start({
         initialState: {
           position: 456,
         },
       })
 
       await waitFor(() => {
-        const bobPeerStates = bobPresence.getPeerStates()
+        const bobPeerStates = bob.presence.getPeerStates()
         const bobPeers = bobPeerStates.getPeers()
 
         expect(bobPeers.length).toBe(1)
         expect(bobPeerStates.getPeerState(bobPeers[0], "position")).toBe(123)
 
-        const alicePeerStates = alicePresence.getPeerStates()
+        const alicePeerStates = alice.presence.getPeerStates()
         const alicePeers = alicePeerStates.getPeers()
 
         expect(alicePeers.length).toBe(1)
@@ -77,41 +88,27 @@ describe("Presence", () => {
 
   describe("heartbeats", () => {
     it("sends heartbeats on the configured interval", async () => {
-      const [alice, bob] = await setup()
-      const aliceHandle = alice.create({
-        test: "doc",
-      })
-      const alicePresence = new Presence<PresenceState>({
-        handle: aliceHandle,
-        userId: "alice",
-        deviceId: "phone",
-      })
-      alicePresence.start({
+      const { alice, bob } = await setup()
+      alice.presence.start({
         initialState: {
           position: 123,
         },
         heartbeatMs: 10,
       })
 
-      const bobHandle = await bob.find(aliceHandle.url)
-      const bobPresence = new Presence<PresenceState>({
-        handle: bobHandle,
-        userId: "bob",
-        deviceId: "phone",
-      })
-      bobPresence.start({
+      bob.presence.start({
         initialState: {
           position: 456,
         },
       })
 
       let hbPeerMsg: PresenceEventHeartbeat
-      bobPresence.on("heartbeat", msg => {
+      bob.presence.on("heartbeat", msg => {
         hbPeerMsg = msg
       })
 
       await waitFor(() => {
-        expect(hbPeerMsg.peerId).toEqual(alice.peerId)
+        expect(hbPeerMsg.peerId).toEqual(alice.repo.peerId)
         expect(hbPeerMsg.type).toEqual("heartbeat")
         expect(hbPeerMsg.userId).toEqual("alice")
       })
@@ -120,43 +117,31 @@ describe("Presence", () => {
     it.skip("delays heartbeats when there is a state update")
   })
 
-  describe("state", () => {
+  describe("broadcast", () => {
     it("sends updates to peers", async () => {
-      const [alice, bob] = await setup()
-      const aliceHandle = alice.create({
-        test: "doc",
-      })
-      const alicePresence = new Presence<PresenceState>({
-        handle: aliceHandle,
-        userId: "alice",
-        deviceId: "phone",
-      })
-      alicePresence.start({
+      const { alice, bob } = await setup()
+      alice.presence.start({
         initialState: {
           position: 123,
         },
       })
 
-      const bobHandle = await bob.find(aliceHandle.url)
-      const bobPresence = new Presence<PresenceState>({
-        handle: bobHandle,
-        userId: "bob",
-        deviceId: "phone",
-      })
-      bobPresence.start({
+      bob.presence.start({
         initialState: {
           position: 456,
         },
       })
 
-      alicePresence.broadcast("position", 213)
+      alice.presence.broadcast("position", 213)
 
       await waitFor(() => {
-        const bobPeerStates = bobPresence.getPeerStates()
+        const bobPeerStates = bob.presence.getPeerStates()
         const bobPeers = bobPeerStates.getPeers()
 
         expect(bobPeers.length).toBe(1)
-        expect(bobPeerStates.getPeerState(alice.peerId, "position")).toBe(213)
+        expect(bobPeerStates.getPeerState(alice.repo.peerId, "position")).toBe(
+          213
+        )
       })
     })
 
