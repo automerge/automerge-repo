f4851cb3db754165b42214df5799eccedb046c60
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-12-10 13:00:18 -0800

    Port dependencies and react-todo/main.tsx

--- examples/react-todo/src/main.tsx
+++ examples/react-todo/src/main.tsx
@@ -1,13 +1,14 @@
 import {
-  DocHandle,
-  Repo,
-  isValidAutomergeUrl,
-  BroadcastChannelNetworkAdapter,
-  WebSocketClientAdapter,
-  IndexedDBStorageAdapter,
-  RepoContext,
+    DocHandle,
+    Repo,
+    isValidAutomergeUrl,
+    BroadcastChannelNetworkAdapter,
+    WebSocketClientAdapter,
+    IndexedDBStorageAdapter,
+    RepoContext,
 } from "@automerge/react"
 
+import { IndexedDbStorage, Subduction } from "@automerge/automerge_subduction"
 import React, { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import ReactDOM from "react-dom/client"
@@ -15,40 +16,42 @@ import { App } from "./App.js"
 import { State } from "./types.js"
 import "./index.css"
 
-const repo = new Repo({
-  network: [
-    new BroadcastChannelNetworkAdapter(),
-    new WebSocketClientAdapter("ws://localhost:3030"),
-  ],
-  storage: new IndexedDBStorageAdapter("automerge-repo-demo-todo"),
-})
-
 declare global {
-  interface Window {
-    handle: DocHandle<unknown>
-    repo: Repo
-  }
+    interface Window {
+        handle: DocHandle<unknown>
+        repo: Repo
+    }
 }
 
-const rootDocUrl = `${document.location.hash.substring(1)}`
-let handle
-if (isValidAutomergeUrl(rootDocUrl)) {
-  handle = await repo.find(rootDocUrl)
-} else {
-  handle = repo.create<State>({ todos: [] })
-}
-const docUrl = (document.location.hash = handle.url)
-window.handle = handle // we'll use this later for experimentation
-window.repo = repo
+;(async () => {
+    const db = await IndexedDbStorage.setup(indexedDB)
+    const subduction = new Subduction(db)
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <RepoContext.Provider value={repo}>
-    <React.StrictMode>
-      <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <Suspense fallback={<div>Loading...</div>}>
-          <App url={docUrl} />
-        </Suspense>
-      </ErrorBoundary>
-    </React.StrictMode>
-  </RepoContext.Provider>
-)
+    const oldDb = new IndexedDBStorageAdapter("automerge-repo-demo-todo")
+    const repo = new Repo({
+        network: [],
+        subduction,
+        storage: oldDb,
+    })
+
+    const rootDocUrl = `${document.location.hash.substring(1)}`
+    let handle
+    if (isValidAutomergeUrl(rootDocUrl)) {
+        handle = await repo.find(rootDocUrl)
+    } else {
+        handle = repo.create<State>({ todos: [] })
+    }
+    const docUrl = (document.location.hash = handle.url)
+
+    ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+        <RepoContext.Provider value={repo}>
+            <React.StrictMode>
+                <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <App url={docUrl} />
+                    </Suspense>
+                </ErrorBoundary>
+            </React.StrictMode>
+        </RepoContext.Provider>
+    )
+})()
