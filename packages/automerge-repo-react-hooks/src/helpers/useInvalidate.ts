00fdd0d6301a5556f041209211bae5637073db0c
Maciek Sakrejda <m.sakrejda@gmail.com>
2025-12-15 08:26:49 -0800

    Presence API tweaks (#540)
    * Rework Presence to return new wrapper objects for peer states
    
       This makes it nice to work with in React and any other system that
       depends on object identity.
    
    * Avoid sending userId/deviceId in hello and goodbye messages
    
    * Move presence code to its own subdirectory and break up into files

--- packages/automerge-repo-react-hooks/src/helpers/useInvalidate.ts
+++
@@ -1,7 +0,0 @@
-import { useCallback, useState } from "react"
-
-export function useInvalidate() {
-  const [, setState] = useState(0)
-  const increment = useCallback(() => setState(value => value + 1), [setState])
-  return increment
-}
