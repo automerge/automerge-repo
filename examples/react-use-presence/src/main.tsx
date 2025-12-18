ca28fdb205680d002d2a7a15fc73af2aabddb2bf
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-12-18 13:08:02 -0800

    rebased and working on presence

--- examples/react-use-presence/src/main.tsx
+++ examples/react-use-presence/src/main.tsx
@@ -8,29 +8,34 @@ import {
   IndexedDBStorageAdapter,
   RepoContext,
 } from "@automerge/react"
+import { IndexedDbStorage, Subduction } from "@automerge/automerge_subduction"
 import { v4 } from "uuid"
 
-const repo = new Repo({
-  storage: new IndexedDBStorageAdapter("use-awareness-example"),
-  network: [new BroadcastChannelNetworkAdapter()],
-})
+;(async () => {
+  const db = await IndexedDbStorage.setup(indexedDB)
+  const repo = new Repo({
+    network: [],
+    subduction: await Subduction.hydrate(db),
+  })
+  await repo.connectToWebSocketPeer(repo.peerId, "//127.0.0.1:8080")
 
-const userId = v4()
+  const userId = v4()
 
-const rootDocUrl = `${document.location.hash.substring(1)}`
-const handle = isValidAutomergeUrl(rootDocUrl)
-  ? await repo.find(rootDocUrl)
-  : repo.create()
+  const rootDocUrl = `${document.location.hash.substring(1)}`
+  const handle = isValidAutomergeUrl(rootDocUrl)
+    ? await repo.find(rootDocUrl)
+    : repo.create()
 
-const docUrl = (document.location.hash = handle.url)
+  const docUrl = (document.location.hash = handle.url)
 
-window.handle = handle // we'll use this later for experimentation
-window.repo = repo
+  window.handle = handle // we'll use this later for experimentation
+  window.repo = repo
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RepoContext.Provider value={repo}>
-    <React.StrictMode>
-      <App userId={userId} url={docUrl} />
-    </React.StrictMode>
-  </RepoContext.Provider>
-)
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <RepoContext.Provider value={repo}>
+      <React.StrictMode>
+        <App userId={userId} url={docUrl} />
+      </React.StrictMode>
+    </RepoContext.Provider>
+  )
+})()
