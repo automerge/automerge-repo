649febe49e42427bc34c0be14675573bd3e269f8
Brooklyn Zelenka <hello@brooklynzelenka.com>
2025-12-18 12:56:36 -0800

    Svelte counter works

--- examples/svelte-counter/src/main.ts
+++ examples/svelte-counter/src/main.ts
@@ -1,10 +1,29 @@
-import "./app.css"
-import App from "./App.svelte"
-import { mount } from "svelte"
+// import "./app.css"
+// import App from "./App.svelte"
+// import { mount } from "svelte"
+// import { setupRepo } from "./lib/repo.js";
+//
+// const repo = await setupRepo();
+//
+// const target = document.getElementById("app")
+// if (target) {
+//   mount(App, { target })
+// }
+// const props = { repo }
+//
+// export default App
 
-const target = document.getElementById("app")
-if (target) {
-  mount(App, { target })
-}
+import "./app.css";
+import App from "./App.svelte";
+import { mount } from "svelte";
+import { setupRepo } from "./lib/repo.js";
 
-export default App
+const target = document.getElementById("app");
+if (!target) throw new Error('Missing mount target: #app');
+
+const repo = await setupRepo();
+
+mount(App, {
+  target,
+  props: { repo },
+});
