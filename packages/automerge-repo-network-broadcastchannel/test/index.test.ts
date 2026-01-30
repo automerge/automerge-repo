a43b06adfcbbda52e6b221b72e75b7f8afcd73e1
Maciek Sakrejda <m.sakrejda@gmail.com>
2026-01-30 13:33:39 -0800

    Avoid waiting for peers indefinitely in BroadcastChannelNetworkAdapter (#561)
    BroadcastChannelNetworkAdapter currently only declares itself ready
    once it has heard from a peer. If no peers ever show up (e.g., no
    others tabs in the same browser), the adapter waits indefinitely. This
    can cause hangs when loading documents.
    
    Instead, add an option to stop waiting for peers after a timeout and
    default it to one second.
    
    Fixes #408

--- packages/automerge-repo-network-broadcastchannel/test/index.test.ts
+++ packages/automerge-repo-network-broadcastchannel/test/index.test.ts
@@ -1,10 +1,11 @@
 import { PeerId } from "@automerge/automerge-repo"
-import { describe, it } from "vitest"
+import { describe, expect, it } from "vitest"
 import {
   runNetworkAdapterTests,
   type SetupFn,
 } from "../../automerge-repo/src/helpers/tests/network-adapter-tests.js"
 import { BroadcastChannelNetworkAdapter } from "../src/index.js"
+import { pause } from "../../automerge-repo/src/helpers/pause.js"
 
 describe("BroadcastChannel", () => {
   const setup: SetupFn = async () => {
@@ -41,5 +42,14 @@ describe("BroadcastChannel", () => {
     c.connect("c" as PeerId)
 
     return Promise.all([aConnect, cShouldNotConnect])
+  })
+
+  it("allows a wait time to be specified in the options and is ready after that even if no peers have connected", async () => {
+    const a = new BroadcastChannelNetworkAdapter({
+      channelName: "a",
+      peerWaitMs: 10,
+    })
+    await pause(10)
+    expect(a.isReady()).toBe(true)
   })
 })
