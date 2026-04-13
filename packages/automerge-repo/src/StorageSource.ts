1627852fd9074e836fdc154638f1ba6a8c9148e6
John Mumm <jtfmumm@gmail.com>
2026-04-13 15:04:37 +0100

    Added shareConfigChanged() to DocumentSource

--- packages/automerge-repo/src/StorageSource.ts
+++ packages/automerge-repo/src/StorageSource.ts
@@ -54,6 +54,8 @@ export class StorageSource implements DocumentSource {
     delete this.#saveFns[documentId]
   }
 
+  shareConfigChanged(): void {}
+
   #makeSaveFn(
     documentId: DocumentId
   ): (payload: DocHandleEncodedChangePayload<any>) => void {
